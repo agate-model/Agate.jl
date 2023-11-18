498ab93237d006399be4980dc44c5e3cc979117d
nanophyto <coralscaper@gmail.com>
2023-11-18 14:24:53 +0000

    sub/super script formatting

--- src/Growth/default.jl
+++ src/Growth/default.jl
@@ -1,33 +1,33 @@
 "
-    PCⱼ = PCᵐᵃˣⱼ * γⁿᵘᵗⱼ *  γˡⁱᵍʰᵗⱼ * fᵗᵉᵐᵖⱼ *  γᶜᵒ²ⱼ
+    PCⱼ = PCⱼᵐᵃˣ * γⱼⁿᵘᵗ *  γⱼˡⁱᵍʰᵗ * fⱼᵗᵉᵐᵖ *  γⱼᶜᵒ²
 
 Carbon-specific growth rate for plankton j (Default MITgcm-DARWIN formulation).
 
 Where: 
-PCᵐᵃˣⱼ = maximum carbon-specific growth rate for plankton j
-γⁿᵘᵗⱼ = nutrient limition
-γˡⁱᵍʰᵗⱼ = light limition
-fᵗᵉᵐᵖⱼ = temperature limitation
+PCᵐᵃˣⱼ = maximum carbon-specific growth rate for plankton j,
+γⁿᵘᵗⱼ = nutrient limition,
+γˡⁱᵍʰᵗⱼ = light limition,
+fᵗᵉᵐᵖⱼ = temperature limitation,
 γᶜᵒ²ⱼ = carbon dioxide limitation
 
 "
-function default_PCⱼ(PCᵐᵃˣⱼ, γⁿᵘᵗⱼ,  γˡⁱᵍʰᵗⱼ, fᵗᵉᵐᵖⱼ,  γᶜᵒ²ⱼ)
-    PCᵐᵃˣⱼ * γⁿᵘᵗⱼ *  γˡⁱᵍʰᵗⱼ * fᵗᵉᵐᵖⱼ *  γᶜᵒ²ⱼ
+function default_PCⱼ(PCⱼᵐᵃˣ, γⱼⁿᵘᵗ,  γⱼˡⁱᵍʰᵗ, fⱼᵗᵉᵐᵖ,  γⱼᶜᵒ²)
+    PCⱼᵐᵃˣ * γⱼⁿᵘᵗ *  γⱼˡⁱᵍʰᵗ * fⱼᵗᵉᵐᵖ *  γⱼᶜᵒ²
 end
 
 
 "
-    γˡⁱᵍʰᵗⱼ = (1 - ℯ^(kˢᵃᵗⱼ*I)) * ℯ^kⁱⁿʰⱼ * nˡⁱᵍʰᵗⱼ
+    γⱼˡⁱᵍʰᵗ = (1 - ℯ^(kⱼˢᵃᵗ*I)) * ℯ^kⱼⁱⁿʰ * nⱼˡⁱᵍʰᵗ
 
 Light limitation for plankton j (Default MITgcm-DARWIN formulation). 
 
 Where: 
-kˢᵃᵗⱼ = half saturation constant of light saturation of plankton j.
-I = irradiance
-kⁱⁿʰⱼ = half saturation constant of light inhibition of plankton j.
-nˡⁱᵍʰᵗⱼ = light penalty term of plankton j
+kⱼˢᵃᵗ = half saturation constant of light saturation of plankton j,
+I = irradiance,
+kⱼⁱⁿʰ = half saturation constant of light inhibition of plankton j,
+nⱼˡⁱᵍʰᵗ = light penalty term of plankton j
 
 "
-function γˡⁱᵍʰᵗⱼ(I, kˢᵃᵗⱼ, kⁱⁿʰⱼ, nˡⁱᵍʰᵗⱼ)
-    (1 - ℯ^(kˢᵃᵗⱼ*I)) * ℯ^kⁱⁿʰⱼ * nˡⁱᵍʰᵗⱼ
+function γⱼˡⁱᵍʰᵗ(I, kⱼˢᵃᵗ, kⱼⁱⁿʰ, nⱼˡⁱᵍʰᵗ)
+    (1 - ℯ^(kⱼˢᵃᵗ*I)) * ℯ^kⱼⁱⁿʰ * nⱼˡⁱᵍʰᵗ
 end
