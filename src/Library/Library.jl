3caab50209223c8f0b5629ae03055cb089182d8a
nanophyto <coralscaper@gmail.com>
2024-09-17 11:26:36 +0100

    Initial folder structure

---
+++ src/Library/Library.jl
@@ -0,0 +1,8 @@
+include("Chlorophyll/Chlorophyll.jl")
+include("Growth/Growth.jl")
+include("Mortality/Mortality.jl")
+include("Nutrients/Nutrients.jl")
+include("Predation/Predation.jl")
+include("Remineralization/Remineralization.jl")
+include("Temperature/Temperature.jl")
+include("Allometry/Allometry.jl")
