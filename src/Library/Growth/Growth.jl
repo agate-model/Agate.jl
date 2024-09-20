ec7ef4a959bc4c7a8811415eae9d692d1bcefb90
nanophyto <coralscaper@gmail.com>
2024-09-20 16:28:57 +0100

    merge sub-files into main file

--- src/Library/Growth/Growth.jl
+++ src/Library/Growth/Growth.jl
@@ -1,6 +1,21 @@
 module Growth
 
-include("default.jl")
+"
+    PCⱼ = PCⱼᵐᵃˣ * γⱼⁿᵘᵗ *  γⱼˡⁱᵍʰᵗ * fⱼᵗᵉᵐᵖ *  γⱼᶜᵒ²
+
+Carbon-specific growth rate for plankton j (Default MITgcm-DARWIN formulation).
+
+Where: 
+PCᵐᵃˣⱼ = maximum carbon-specific growth rate for plankton j,
+γⁿᵘᵗⱼ = nutrient limition,
+γˡⁱᵍʰᵗⱼ = light limition,
+fᵗᵉᵐᵖⱼ = temperature limitation,
+γᶜᵒ²ⱼ = carbon dioxide limitation
+
+"
+function default_PCⱼ(PCⱼᵐᵃˣ, γⱼⁿᵘᵗ,  γⱼˡⁱᵍʰᵗ, fⱼᵗᵉᵐᵖ,  γⱼᶜᵒ²)
+    PCⱼᵐᵃˣ * γⱼⁿᵘᵗ *  γⱼˡⁱᵍʰᵗ * fⱼᵗᵉᵐᵖ *  γⱼᶜᵒ²
+end
 
 export
     default_PCⱼ
--- src/Library/Nutrients/Nutrients.jl
+++ src/Library/Nutrients/Nutrients.jl
@@ -1,7 +1,20 @@
 module Nutrients
 
-include("monod.jl")
-include("droop.jl")
+"
+    N / (kₙ + N)
+
+Monod formulation of nutrient limitation, which is based on Michaelis-Menten enzyme kinetics. 
+
+Where: 
+R = nutrient (e.g. N, P, Si)
+kᵣ = nutrient half saturation constant
+
+Note that sometimes this formulation is also used for Predation.
+
+"
+function monod_limitation(R, kᵣ)
+    R / (kᵣ + R)
+end
 
 export
     monod_limitation
