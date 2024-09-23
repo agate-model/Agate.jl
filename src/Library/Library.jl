91a2f53d977adac444f06242be13cd6f2e904bbf
nanophyto <coralscaper@gmail.com>
2024-09-23 09:57:53 +0100

    remove library subfolders

--- src/Library/Library.jl
+++ src/Library/Library.jl
@@ -4,15 +4,15 @@ A library of modules to create marine biogeochemical models
 """
 module Library
 
-include("Allometry/Allometry.jl")
-include("Chlorophyll/Chlorophyll.jl")
-include("Growth/Growth.jl")
-include("Light/Light.jl")
-include("Mortality/Mortality.jl")
-include("Nutrients/Nutrients.jl")
-include("Predation/Predation.jl")
-include("Remineralization/Remineralization.jl")
-include("Temperature/Temperature.jl")
+include("allometry.jl")
+include("chlorophyll.jl")
+include("growth.jl")
+include("light.jl")
+include("mortality.jl")
+include("nutrients.jl")
+include("predation.jl")
+include("remineralization.jl")
+include("temperature.jl")
 
 using .Allometry
 using .Chlorophyll
