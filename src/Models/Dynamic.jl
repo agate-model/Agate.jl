9d4091adefe39cc0f473d3b353e6ae08de0968aa
radka-j <r.jersakova@gmail.com>
2024-09-24 17:23:19 +0100

    add docstrings, handle case when priors are a NamedTuple

--- src/Models/Dynamic.jl
+++ src/Models/Dynamic.jl
@@ -1,24 +1,29 @@
 """
-Module to dynamically create Oceananigans.Biogeochmistry types.
+Module to dynamically create AbstractContinuousFormBiogeochemistry types (imported from
+Oceananigans.Biogeochemistry).
 """
 
 module Dynamic
 
 using Oceananigans.Biogeochemistry: AbstractContinuousFormBiogeochemistry
 
-# all Agate functions have to be in scope to evaluate expressions that might contain them
-include("Library.jl")
-
 export create_struct, construct_bgc_model
 
 
 """
-Create struct of type AbstractContinuousFormBiogeochemistry...
+    create_struct(struct_name, priors::Dict) -> AbstractContinuousFormBiogeochemistry
+
+Create a struct of type AbstractContinuousFormBiogeochemistry. Uses field names and default
+values defined in priors (which can be, for example, a Dict or NamedTuple).
+
+# Arguments
+- `struct_name`: name for the new struct
+- `priors`: named sequence of values of the form (field name = default value, ...)
 """
-function create_struct(priors, struct_name)
+function create_struct(struct_name, priors)
 
     fields = []
-    for (k,v) in priors
+    for (k,v) in pairs(priors)
         exp = :($k = $v)
         push!(fields, exp)
     end
@@ -32,21 +37,51 @@ function create_struct(priors, struct_name)
 end
 
 
+"""
+    construct_bgc_model(bgc_struct, tracers, auxiliary_fields=[])
 
+Add required methods to bgc_model struct:
+    - method per tracer
+    - required_biogeochemical_tracers
+    - required_biogeochemical_auxiliary_fields
+Optionally can include auxiliary fields.
+
+# Arguments
+- `bgc_struct``: struct (of type AbstractContinuousFormBiogeochemistry)
+- `tracers`: Dict of named function expressions of the form (name = expression, ...)
+- `auxiliary_fields`: optional iterable of auxiliary field variables
+
+# Example
+```julia
+struct LV <: AbstractContinuousFormBiogeochemistry
+    α
+    β
+    δ
+    γ
+end
+bgc_struct = LV(2/3, 4/3, 1, 1)
+tracers = Dict(
+    "F" => :(α*F - β*F*R),
+    "R" => :(-γ*R + δ*F*R)
+)
+model = construct_bgc_model(bgc_struct, tracers)
+```
 """
-Construct a concrete Oceananigans biogeochemical model instance from a dictionary of
-tracer equations. Optionally can include auxiliary fields...
-"""
-function construct_bgc_model(tracers, bgc_struct; auxiliary_fields=[])
+function construct_bgc_model(bgc_struct, tracers; auxiliary_fields=[])
 
     # all BGC models require these base variables
     base_vars = [:x, :y, :z, :t]
-    tracer_vars = Symbol.(keys(tracers))
+    # use collect here in case tracers are NamedTuple rather than Dict
+    tracer_vars = Symbol.(collect(keys(tracers)))
     aux_field_vars = Symbol.(auxiliary_fields)
     all_state_vars = vcat(base_vars , tracer_vars, aux_field_vars)
 
-    eval(:(required_biogeochemical_tracers(::$(typeof(bgc_struct))) = Tuple(tracer_vars)))
-    eval(:(required_biogeochemical_auxiliary_fields(::$(typeof(bgc_struct))) = Tuple(aux_field_vars)))
+    eval(:(required_biogeochemical_tracers(
+        ::$(typeof(bgc_struct))) = Tuple(tracer_vars)
+        ))
+    eval(:(required_biogeochemical_auxiliary_fields(
+        ::$(typeof(bgc_struct))) = Tuple(aux_field_vars)
+        ))
 
     # the variable expressions are evaluated inside the tracer methods built below
     method_vars = []
@@ -55,7 +90,7 @@ function construct_bgc_model(tracers, bgc_struct; auxiliary_fields=[])
         push!(method_vars, exp)
     end
 
-    for (tracer_name, func_expression) in tracers
+    for (tracer_name, func_expression) in pairs(tracers)
         tracer_method = quote
             function (bgc::$(typeof(bgc_struct)))(::Val{Symbol($tracer_name)}, $(all_state_vars...))
                 $(method_vars...)
