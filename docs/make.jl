11122f244151c30f4564cd0ba53cbb303033ded0
radka-j <r.jersakova@gmail.com>
2024-10-08 10:15:24 +0100

    format docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,26 +1,15 @@
-push!(LOAD_PATH,"../src/")
+push!(LOAD_PATH, "../src/")
 using Documenter, Agate
 
 About = "Introduction" => "index.md"
 
-DevDocs = [
-	"Documentation of Agate.jl internals" => [
-		"devdocs/growth.md"
-	]
-]
+DevDocs = ["Documentation of Agate.jl internals" => ["devdocs/growth.md"]]
 
-PAGES = [
-    About,
-    DevDocs
-    ]
+PAGES = [About, DevDocs]
 
-makedocs(
+makedocs(;
     sitename="Agate.jl Documentation",
-    format = Documenter.HTML(
-        prettyurls = get(ENV, "CI", nothing) == "true"
-    )
+    format=Documenter.HTML(; prettyurls=get(ENV, "CI", nothing) == "true"),
 )
 
-deploydocs(
-    repo = "https://github.com/agate-model/Agate.jl.git",
-)
+deploydocs(; repo="https://github.com/agate-model/Agate.jl.git")
