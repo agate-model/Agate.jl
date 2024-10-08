e2d0f5e45cacd4a645e909cd054bc760eb759858
radka-j <r.jersakova@gmail.com>
2024-10-08 15:24:40 +0100

    merge main

--- src/Agate.jl
+++ src/Agate.jl
@@ -5,10 +5,7 @@ include("Library/Library.jl")
 include("Models/Models.jl")
 using .Models
 
-<<<<<<< HEAD
 export create_box_model, run_box_model
-=======
->>>>>>> main
 export create_bgc_struct, add_bgc_methods
 
 end # module
