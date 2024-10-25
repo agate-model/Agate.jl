0a17f2e2616a3bd9d2910f1cf1eb3ab882cbb2c8
radka-j <r.jersakova@gmail.com>
2024-10-25 16:44:51 +0200

    add create_bgc_model placeholder

--- src/Agate.jl
+++ src/Agate.jl
@@ -1,11 +1,14 @@
 module Agate
 
 include("box_model.jl")
+include("bgc_model.jl")
 include("Library/Library.jl")
 include("Models/Models.jl")
 using .Models
 
-export create_box_model, run_box_model
 export define_tracer_functions
+export create_bgc_model
+export create_box_model, run_box_model
+
 
 end # module
