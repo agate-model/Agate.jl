2f526afb0d1611e9ba82c69624b442028f6b9ca6
radka-j <r.jersakova@gmail.com>
2024-10-25 17:12:22 +0200

    forgot to commit new file

---
+++ src/bgc_model.jl
@@ -0,0 +1,18 @@
+using OceanBioME: Biogeochemistry
+
+export create_bgc_model
+
+"""
+# Arguments
+- `bgc_tracers`: biogeochemistry model tracers, a subtype of AbstractContinuousFormBiogeochemistry
+    e.g., returned by `Agate.define_tracer_functions`
+
+NOTE: currently a placeholder
+
+# Keywords
+- `grid`: the geometry to build the model on defined as an Oceananigans grid object
+- `light_attenuation`: model for attenuation of PAR through water
+"""
+function create_bgc_model(bgc_tracers, grid, light_attenuation=nothing)
+    return Biogeochemistry(bgc_tracers; light_attenuation=light_attenuation)
+end
