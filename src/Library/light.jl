f0811b48331b897a6c6a13fa8f62638ed39d36e1
radka-j <r.jersakova@gmail.com>
2024-10-28 17:41:26 +0000

    mv all cyclical PAR functions to light module

--- src/Library/light.jl
+++ src/Library/light.jl
@@ -1,11 +1,16 @@
 """
 Modules related to photosynthetically available radiation (PAR)
-
 """
 
 module Light
 
+using Oceananigans
+using Oceananigans: Clock
 using Oceananigans.Units
+using Oceananigans.Fields: FunctionField, compute!
+
+import Oceananigans.Biogeochemistry:
+    update_biogeochemical_state!, biogeochemical_auxiliary_fields
 
 const year = years = 365day
 
@@ -14,7 +19,7 @@ const year = years = 365day
 
 Time-dependent cyclical PAR at depth `z` (suitable for use with box models).
 """
-function cyclical_PAR(t, z)
+function cyclical_PAR(z, t)
     PAR⁰ =
         60 *
         (1 - cos((t + 15days) * 2π / year)) *
@@ -22,8 +27,50 @@ function cyclical_PAR(t, z)
     return PAR⁰ * exp(0.2 * z)
 end
 
-cyclical_PAR(; z) = t -> cyclical_PAR(t, z)
+cyclical_PAR(; z) = t -> cyclical_PAR(z, t)
 
-export cyclical_PAR
+"""
+Time-dependent cyclical PAR at depth `z` (suitable for use with column models).
+"""
+function cyclical_PAR(x, y, z, t)
+    return cyclical_PAR(z, t)
+end
+
+"""
+Light module for cyclical PAR (can be used with box or column models).
+
+# Fields
+- `field`: Oceananigans.FunctionField
+"""
+struct CyclicalPhotosyntheticallyActiveRadiation
+    field
+end
+
+"""
+    CyclicalPhotosyntheticallyActiveRadiation() -> DataType
+
+# Keywords
+- `grid`: the geometry to build the model on defined as an Oceananigans grid object
+- `PAR_f`: a time dependant PAR function (defaults to `Agate.Library.Light.cyclical_PAR`)
+"""
+function CyclicalPhotosyntheticallyActiveRadiation(;
+    grid, PAR_f=cyclical_PAR(; z=-10)
+)
+    clock = Clock(; time=zero(grid))
+    PAR = FunctionField{Center,Center,Center}(PAR_f, grid; clock)
+    return CyclicalPhotosyntheticallyActiveRadiation(PAR)
+end
+
+function update_biogeochemical_state!(model, PAR::CyclicalPhotosyntheticallyActiveRadiation)
+    PAR.field.clock.time = model.clock.time
+    compute!(PAR.field)
+    return nothing
+end
+
+function biogeochemical_auxiliary_fields(par::CyclicalPhotosyntheticallyActiveRadiation)
+    return (PAR=par.field,)
+end
+
+export cyclical_PAR, CyclicalPhotosyntheticallyActiveRadiation
 
 end # module
--- src/bgc_model.jl
+++ src/bgc_model.jl
@@ -3,17 +3,7 @@ include("Library/light.jl")
 using .Light
 
 using OceanBioME
-using Oceananigans
-using Oceananigans: Clock
-
-using Oceananigans.Units
-using Oceananigans.Fields: FunctionField, compute!
-
 using OceanBioME: Biogeochemistry
-
-import Oceananigans: set!
-import Oceananigans.Biogeochemistry:
-    update_biogeochemical_state!, biogeochemical_auxiliary_fields
 
 export create_bgc_model
 
@@ -29,42 +19,7 @@ export create_bgc_model
 function create_bgc_model(
     bgc_tracers;
     grid=BoxModelGrid(),
-    light_attenuation=BoxPhotosyntheticallyActiveRadiation(; grid=grid),
+    light_attenuation=CyclicalPhotosyntheticallyActiveRadiation(; grid=grid),
 )
     return Biogeochemistry(bgc_tracers; light_attenuation=light_attenuation)
-end
-
-"""
-Light module for use with Box models
-
-# Fields
-- `field`: Oceananigans.FunctionField
-"""
-struct BoxPhotosyntheticallyActiveRadiation
-    field
-end
-
-"""
-    BoxPhotosyntheticallyActiveRadiation() -> DataType
-
-# Keywords
-- `grid`: the geometry to build the model on defined as an Oceananigans grid object
-- `PAR_f`: a time dependant PAR function (defaults to `Agate.Library.Light.cyclical_PAR`)
-"""
-function BoxPhotosyntheticallyActiveRadiation(;
-    grid=BoxModelGrid(), PAR_f=cyclical_PAR(; z=-10)
-)
-    clock = Clock(; time=zero(grid))
-    PAR = FunctionField{Center,Center,Center}(PAR_f, grid; clock)
-    return BoxPhotosyntheticallyActiveRadiation(PAR)
-end
-
-function update_biogeochemical_state!(model, PAR::BoxPhotosyntheticallyActiveRadiation)
-    PAR.field.clock.time = model.clock.time
-    compute!(PAR.field)
-    return nothing
-end
-
-function biogeochemical_auxiliary_fields(par::BoxPhotosyntheticallyActiveRadiation)
-    return (PAR=par.field,)
 end
