64bfdd4374998d16c8c423d839316a5d8146a06b
radka-j <r.jersakova@gmail.com>
2024-10-30 14:19:52 +0000

    rm create_bgc_model function

--- src/Agate.jl
+++ src/Agate.jl
@@ -1,6 +1,5 @@
 module Agate
 
-# include("bgc_model.jl")
 include("simulate.jl")
 
 include("Library/Library.jl")
@@ -9,7 +8,6 @@ include("Models/Models.jl")
 using .Models
 
 export define_tracer_functions
-# export create_bgc_model
 export run_simulation
 
 end # module
