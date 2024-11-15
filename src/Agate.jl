51d90f87289eb85995ea267ffd99b879ab06d3b5
radka-j <r.jersakova@gmail.com>
2024-11-15 10:47:36 +0000

    rm outdated function

--- src/Agate.jl
+++ src/Agate.jl
@@ -3,11 +3,8 @@ module Agate
 include("Library/Library.jl")
 include("Models/Models.jl")
 
-include("Models/Parameters.jl")
-
 using .Models
 
-export add_plankton_tracers
 export define_tracer_functions
 
 end # module
--- src/Models/Models.jl
+++ src/Models/Models.jl
@@ -1,7 +1,10 @@
 module Models
 
 include("Dynamic.jl")
+include("Models/Parameters.jl")
+
 using .Dynamic
+using .Parameters
 
 export define_tracer_functions
 
