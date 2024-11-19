c04b39ce288cb300ff11c1aa3d1799373a8dc28b
radka-j <r.jersakova@gmail.com>
2024-11-19 13:26:42 +0000

    update example

--- examples/emergent_4P/example.jl
+++ examples/emergent_4P/example.jl
@@ -1,14 +1,12 @@
 using Oceananigans.Units
-
-include("emergent_functions.jl")
-include("helper_functions.jl")
+using Agate
 
 defined_parameters = Dict(
     "P" => Dict(
         "n" => 2,
         "min_volume" => 1,
         "max_volume" => 10,
-        "splitting" => log_splitting,
+        "splitting" => "log_splitting",
         "growth_a" => 1,
         "growth_b" => 1,
         "protection" => 0,
@@ -27,7 +25,7 @@ defined_parameters = Dict(
         "n" => 2,
         "min_volume" => 10,
         "max_volume" => 100,
-        "splitting" => linear_splitting,
+        "splitting" => "linear_splitting",
         "growth_a" => 0,
         "growth_b" => 0,
         "protection" => 1,
@@ -44,54 +42,38 @@ defined_parameters = Dict(
     ),
 )
 
-# Generate intermediate parameters 
-intermediate_parameters = split_size_parameters(defined_parameters)
-
-# Dictionary of emergent functions
-emergent_functions = Dict(
-    "maximum_growth_rate" => (dummy_emergent_growth, ["growth_a", "growth_b", "volume"]),
-    "palatability" =>
-        (dummy_emergent_palat, ["volume", "optimum_predator_prey_ratio", "protection"]),
-    "maximum_predation_rate" =>
-        (dummy_emergent_predation_rate, ["predation_rate_a", "predation_rate_b", "volume"]),
-    "nitrogen_half_saturation" => (
-        dummy_emergent_nitrogen_half_saturation,
-        ["nitrogen_half_saturation_a", "nitrogen_half_saturation_b", "volume"],
-    ),
-    "assimilation_efficiency" =>
-        (dummy_emergent_assimilation_efficiency, ["assimilation_efficiency"]),
-)
-
-emergent_parameters = analyze_and_merge(emergent_functions, intermediate_parameters)
+emergent_parameters = compute_darwin_parameters(defined_parameters)
 
 # Display the results dictionary
 println("All results:", emergent_parameters)
 
 #check named arrays:
 
-#growth rate of P2:
+# growth rate of P2:
 println(emergent_parameters["maximum_growth_rate"]["P2"])
 
-#palability of P2 to P1:
+println(emergent_parameters["palatability"])
+
+# palability of P2 to P1:
 println(emergent_parameters["palatability"]["P1", "P2"])
 
-#palability of Z1 to P1:
+# palability of Z1 to P1:
 println(emergent_parameters["palatability"]["Z1", "P2"])
 
-#assimilation efficiency of Z1 to P1:
-#println(emergent_parameters["assimilation_efficiency"]["Z1", "P2"]) #currently broken
+# #assimilation efficiency of Z1 to P1:
+println(emergent_parameters["assimilation_efficiency"]["Z1", "P2"]) #curreassimintly broken
 
-#just a test - redefine a palat link to be something else:
+# just a test - redefine a palat link to be something else:
 emergent_parameters["palatability"]["Z1", "P2"] = 10
 
-#for simplicity define the biogeochemistry dict seperately
+# for simplicity define the biogeochemistry dict seperately
 biogeochemistry_parameters = Dict(
     "detritus_remineralization" => 0.1213 / day,
     "feeding_export_poc_doc_fraction" => 0.5,
     "mortality_export_fraction" => 0.5,
 )
 
-#merge into one dictionary 
+#merge into one dictionary
 parameters = merge(biogeochemistry_parameters, emergent_parameters)
 
 println(parameters)
