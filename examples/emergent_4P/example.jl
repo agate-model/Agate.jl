54d4e86bc2fce269d38635c2ecff8d5f703b4180
radka-j <r.jersakova@gmail.com>
2024-11-25 11:15:35 +0000

    update example

--- examples/emergent_4P/example.jl
+++ examples/emergent_4P/example.jl
@@ -6,6 +6,8 @@ using Agate
 # NOTE: assimilation efficiency has to create an array of values rather than compute anything --> is there a better way to do that?
 # Q: related to assimilation - do the Zs ever have assimilation related to each other? what if there was a third species, what would that look like?
 
+# NOTE: don't need to declare units as per second since this is the default time unit i.e., 1 == 1/sec
+
 defined_parameters = Dict(
     "P" => Dict(
         "n" => 2,
@@ -14,15 +16,15 @@ defined_parameters = Dict(
         "allometry" => Dict(
             "maximum_growth_rate" => Dict("a" => 1, "b" => 1),
             "nitrogen_half_saturation" => Dict("a" => 1, "b" => 1),
-            # "maximum_predation_rate" => Dict("a" => 0, "b" => 0),
+            "maximum_predation_rate" => Dict("a" => 0, "b" => 0),
         ),
         "palatability" => Dict("optimum_predator_prey_ratio" => 0, "protection" => 0),
         "assimilation_efficiency" =>
             Dict("can_be_eaten" => 1, "can_eat" => 0, "assimilation_efficiency" => 0),
         # anything else goes here
         "linear_mortality" => 8e-7 / second,
-        # "holling_half_saturation" => 0,
-        # "quadratic_mortality" => 0,
+        "holling_half_saturation" => 0,
+        "quadratic_mortality" => 0,
         "alpha" => 0.1953 / day,
     ),
     "Z" => Dict(
@@ -33,8 +35,8 @@ defined_parameters = Dict(
             "splitting" => "linear_splitting",
         ),
         "allometry" => Dict(
-            # "maximum_growth_rate" => Dict("a" => 0, "b" => 0),
-            # "nitrogen_half_saturation" => Dict("a" => 0, "b" => 0),
+            "maximum_growth_rate" => Dict("a" => 0, "b" => 0),
+            "nitrogen_half_saturation" => Dict("a" => 0, "b" => 0),
             "maximum_predation_rate" => Dict("a" => 1, "b" => 1),
         ),
         "palatability" => Dict("optimum_predator_prey_ratio" => 10, "protection" => 1),
@@ -42,11 +44,11 @@ defined_parameters = Dict(
             "can_be_eaten" => 0, "can_eat" => 1, "assimilation_efficiency" => 0.32
         ),
         # anything else goes here
-        "linear_mortality" => 1e-6 / second,
+        "linear_mortality" => 8e-7 / second,
         "holling_half_saturation" => 5.0,
-        "quadratic_mortality" => 1e-6,
+        "quadratic_mortality" => 1e-6 / second,
         # this should be 0
-        # "alpha" => 1e-99 / day,
+        "alpha" => 1e-99 / day,
     ),
 )
 
@@ -57,25 +59,25 @@ println(emergent_parameters)
 # NOTE: eventually the emergent functions will be updated to true ones
 # that's why we don't include this in tests
 
-# include(joinpath("..", "N2P2ZD", "tracers.jl"))
+include(joinpath("..", "N2P2ZD", "tracers.jl"))
 
-# plankton_order = ["P1", "P2", "Z1", "Z2"]
+plankton_order = ["P1", "P2", "Z1", "Z2"]
 
-# for (key, params) in emergent_parameters
-#     # start with arrays of values
-#     if !(key ∈ ["assimilation_efficiency_matrix", "palatability_matrix", "volume"])
-#         comparison = all(parameters[Symbol(key)] .== [params[p] for p in plankton_order])
-#         println(key, " values are the same: ", comparison)
-#         # matrices of values -> compare row at a time
-#     elseif !(key == "volume")
-#         for (i, p) in enumerate(plankton_order)
-#             emergent_row = params[p, :]
-#             true_row = parameters[Symbol(replace(key, "_matrix" => ""))][i, :]
-#             comparison = all(true_row .== [emergent_row[p] for p in plankton_order])
-#             println(key, " ", p, " values are the same: ", comparison)
-#         end
-#     end
-# end
+for (key, params) in emergent_parameters
+    # start with arrays of values
+    if !(key ∈ ["assimilation_efficiency_matrix", "palatability_matrix", "volumes"])
+        comparison = all(parameters[Symbol(key)] .== [params[p] for p in plankton_order])
+        println(key, " values are the same: ", comparison)
+        # matrices of values -> compare row at a time
+    elseif !(key == "volumes")
+        for (i, p) in enumerate(plankton_order)
+            emergent_row = params[p, :]
+            true_row = parameters[Symbol(replace(key, "_matrix" => ""))][i, :]
+            comparison = all(true_row .== [emergent_row[p] for p in plankton_order])
+            println(key, " ", p, " values are the same: ", comparison)
+        end
+    end
+end
 
 # # for simplicity define the biogeochemistry dict seperately
 # biogeochemistry_parameters = Dict(
