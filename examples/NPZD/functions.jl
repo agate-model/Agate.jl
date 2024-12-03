4e1e4a2c908370817d37d6dda11ff8c2c0dfcde0
nanophyto <coralscaper@gmail.com>
2024-12-03 10:23:55 +0000

    fix function names in NPZD such that they match function names in library

--- examples/NPZD/functions.jl
+++ examples/NPZD/functions.jl
@@ -1,21 +1,22 @@
-menden_limitation(R, k) = R / (k + R)
+monod_limitation(R, k) = R / (k + R) 
+holling_type_2(R, k) = R / (k + R) 
 
 Q₁₀(T) = 1.88^(T / 10) # T in °C
 
 smith_light_limitation(PAR, α, μ₀) = α * PAR / sqrt(μ₀^2 + α^2 * PAR^2)
 
-remineralization(D, r) = r * D
+idealized_remineralization(D, r) = r * D
 
-function photosynthetic_growth(N, P, PAR, μ₀, kₙ, α)
-    return μ₀ * menden_limitation(N, kₙ) * smith_light_limitation(PAR, α, μ₀) * P
+function idealized_photosynthetic_growth(N, P, PAR, μ₀, kₙ, α)
+    return μ₀ * monod_limitation(N, kₙ) * smith_light_limitation(PAR, α, μ₀) * P
 end
 
-predation_loss(P, Z, gₘₐₓ, kₚ) = gₘₐₓ * menden_limitation(P^2, kₚ^2) * Z
+idealized_predation_loss(P, Z, gₘₐₓ, kₚ) = gₘₐₓ * holling_type_2(P^2, kₚ^2) * Z
 
-predation_gain(P, Z, β, gₘₐₓ, kₚ) = β * gₘₐₓ * menden_limitation(P^2, kₚ^2) * Z
+idealized_predation_gain(P, Z, β, gₘₐₓ, kₚ) = β * gₘₐₓ * holling_type_2(P^2, kₚ^2) * Z
 
-function predation_assimilation_loss(P, Z, β, gₘₐₓ, kₚ)
-    return (1 - β) * gₘₐₓ * menden_limitation(P^2, kₚ^2) * Z
+function idealized_predation_assimilation_loss(P, Z, β, gₘₐₓ, kₚ)
+    return (1 - β) * gₘₐₓ * holling_type_2(P^2, kₚ^2) * Z
 end
 
 linear_loss(P, l) = l * P
