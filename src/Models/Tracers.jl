542a5615dc314ebdeca3923be6cdb696eecca85a
radka-j <r.jersakova@gmail.com>
2024-12-10 18:08:28 +0000

    rm phyto/zoo_dt functions

--- src/Models/Tracers.jl
+++ src/Models/Tracers.jl
@@ -71,22 +71,42 @@ Build expression for a simplified phytoplankton growth function....
 # Arguments
 - `plankton_array`: names of all the plankton in the ecosystem expressed as Symbols, e.g.:
     `[:P1, :P2, :Z1, :Z2]`
+
+# Notes on args in the expression
+- `plankton_name`: The name of the plankton for which the rate of change is estimated
+- `N`: Nitrogen
+- `P`: NamedArray which includes all plankton
+- `linear_mortality`: NamedArray of all plankton linear mortality rates
+- `quadratic_mortality`: ....
+- `maximum_growth_rate`: NamedArray of all plankton maximum growth rates
+- `holling_half_saturation`: NamedArray of all plankton predation half saturation constants
+- `maximum_predation_rate`: NamedArray of all plankton maximum predation rates
+- `palatability`: NamedArray of all plankton palatabilities where:
+    - each row is a predator
+    - each column is a prey
+    - values are accessed as `palat[predator, prey]`
+    - for a non-predator [i,:]=0
 """
-function simplified_phytoplankton_growth(plankton_array, name)
-    return :(phytoplankton_dt(
-        $name,
-        N,
-        NamedArray([$(plankton_array...)], $(String.(plankton_array))),
-        PAR,
-        linear_mortality,
-        quadratic_mortality,
-        maximum_growth_rate,
-        holling_half_saturation,
-        nitrogen_half_saturation,
-        alpha,
-        maximum_predation_rate,
-        palatability_matrix,
-    ))
+function simplified_phytoplankton_growth(plankton_array, plankton_name)
+    plankton_symbol = Symbol(plankton_name)
+    return :(
+        idealized_photosynthetic_growth(
+            N,
+            $(plankton_symbol),
+            PAR,
+            maximum_growth_rate[$plankton_name],
+            nitrogen_half_saturation[$plankton_name],
+            alpha[$plankton_name],
+        ) - summed_predation_loss(
+            $plankton_name,
+            NamedArray([$(plankton_array...)], $(String.(plankton_array))),
+            maximum_predation_rate,
+            holling_half_saturation,
+            palatability_matrix,
+        ) - linear_loss($(plankton_symbol), linear_mortality[$plankton_name]) -
+        quadratic_loss($(plankton_symbol), quadratic_mortality[$plankton_name])
+    )
+
 end
 
 """
@@ -95,18 +115,39 @@ Build expression for simplified zooplankton growth function...
 # Arguments
 - `plankton_array`: names of all the plankton in the ecosystem expressed as Symbols, e.g.:
     `[:P1, :P2, :Z1, :Z2]`
+
+# Notes on args in the expression
+- `plankton_name`: The name of the plankton for which the rate of change is estimated
+- `P`: NamedArray which includes all plankton
+- `linear_mortality`: NamedArray of all plankton linear mortality rates
+- `quadratic_mortality`: ....
+- `holling_half_saturation`: NamedArray of all plankton predation half saturation constants
+- `maximum_predation_rate`: NamedArray of all plankton maximum predation rates
+- `assimilation efficiency`: NamedArray of all plankton assimilation efficiencies where:
+    - each row is a predator
+    - each column is a prey
+    - values are accessed as `palat[predator, prey]`
+    - for a non-predator [i,:]=0
+- `palatability`: NamedArray of all plankton palatabilities where:
+    - each row is a predator
+    - each column is a prey
+    - values are accessed as `palat[predator, prey]`
+    - for a non-predator [i,:]=0
 """
-function simplified_zooplankton_growth(plankton_array, name)
-    return :(zooplankton_dt(
-        $name,
-        NamedArray([$(plankton_array...)], $(String.(plankton_array))),
-        linear_mortality,
-        quadratic_mortality,
-        holling_half_saturation,
-        maximum_predation_rate,
-        assimilation_efficiency_matrix,
-        palatability_matrix,
-    ))
+function simplified_zooplankton_growth(plankton_array, plankton_name)
+    plankton_symbol = Symbol(plankton_name)
+    return :(
+        summed_predation_gain(
+            $plankton_name,
+            NamedArray([$(plankton_array...)], $(String.(plankton_array))),
+            assimilation_efficiency_matrix,
+            maximum_predation_rate,
+            holling_half_saturation,
+            palatability_matrix,
+        ) - linear_loss($(plankton_symbol), linear_mortality[$plankton_name]) -
+        quadratic_loss($(plankton_symbol), quadratic_mortality[$plankton_name])
+    )
+
 end
 
 end # module
