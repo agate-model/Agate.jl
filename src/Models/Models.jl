038a26debaf5ca45b7a7f8c3b4d95433ea1b644a
radka-j <r.jersakova@gmail.com>
2024-12-10 13:35:59 +0000

    add tracers module

--- src/Models/Models.jl
+++ src/Models/Models.jl
@@ -2,9 +2,11 @@ module Models
 
 include("Biogeochemistry.jl")
 include("Parameters.jl")
+include("Tracers.jl")
 
 using .Biogeochemistry
 using .Parameters
+using .Tracers
 
 export compute_darwin_parameters
 export define_tracer_functions
