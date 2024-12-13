6c44699d7f05ddfa90447bc62d2f82493e77b55e
radka-j <r.jersakova@gmail.com>
2024-12-13 11:13:23 +0000

    clean up test, rm replicated code from examples

--- test/test_constructor.jl
+++ test/test_constructor.jl
@@ -1,16 +1,13 @@
 using Agate
 
 @testset "Models.Constructor" begin
-    # N2P2ZD model
-    include(joinpath("..", "examples", "emergent_4P", "tracers.jl"))
-
-    # TODO: eventually use constructor here
+    # N2P2ZD model defined using low level syntax
     include(joinpath("..", "examples", "N2P2ZD", "tracers.jl"))
-
     model = N2P2ZD()
-    # model_emergent = N2P2ZD_emergent()
-    N2P2ZD_emergent = construct_NPZD_instance()
-    model_emergent = N2P2ZD_emergent()
+
+    # N2P2ZD model constructed from emergent parameters
+    N2P2ZD_constructed = construct_NPZD_instance()
+    model_constructed = N2P2ZD_constructed()
 
     P1 = 0.01
     P2 = 0.01
@@ -20,23 +17,23 @@ using Agate
     D = 1
     PAR = 100
 
-    @test !iszero(model_emergent(Val(:N), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR))
-    @test !iszero(model_emergent(Val(:D), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR))
-    @test !iszero(model_emergent(Val(:P1), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR))
-    @test !iszero(model_emergent(Val(:P2), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR))
-    @test !iszero(model_emergent(Val(:Z1), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR))
-    @test !iszero(model_emergent(Val(:Z2), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR))
+    @test !iszero(model_constructed(Val(:N), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR))
+    @test !iszero(model_constructed(Val(:D), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR))
+    @test !iszero(model_constructed(Val(:P1), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR))
+    @test !iszero(model_constructed(Val(:P2), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR))
+    @test !iszero(model_constructed(Val(:Z1), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR))
+    @test !iszero(model_constructed(Val(:Z2), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR))
 
-    @test model_emergent(Val(:N), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR) ==
+    @test model_constructed(Val(:N), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR) ==
         model(Val(:N), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR)
-    @test model_emergent(Val(:D), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR) ==
+    @test model_constructed(Val(:D), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR) ==
         model(Val(:D), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR)
-    @test model_emergent(Val(:P1), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR) ==
+    @test model_constructed(Val(:P1), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR) ==
         model(Val(:P1), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR)
-    @test model_emergent(Val(:P2), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR) ==
+    @test model_constructed(Val(:P2), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR) ==
         model(Val(:P2), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR)
-    @test model_emergent(Val(:Z1), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR) ==
+    @test model_constructed(Val(:Z1), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR) ==
         model(Val(:Z1), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR)
-    @test model_emergent(Val(:Z2), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR) ==
+    @test model_constructed(Val(:Z2), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR) ==
         model(Val(:Z2), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR)
 end
