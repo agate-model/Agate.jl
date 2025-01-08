9cc152f68dd8b010f3daa5b9fd8ef9673a8a99d4
nanophyto <coralscaper@gmail.com>
2025-01-08 17:13:44 +0000

    change volume to diameter in dummy functions as well

--- examples/emergent_4P/emergent_functions.jl
+++ examples/emergent_4P/emergent_functions.jl
@@ -1,12 +1,12 @@
 
-function dummy_emergent_growth(growth_a::Real, growth_b::Real, volume::Real)
+function dummy_emergent_growth(growth_a::Real, growth_b::Real, diameter::Real)
     rate = 0.0
     if growth_a == 0
         return rate
     else
-        if volume == 1
+        if diameter == 1
             rate = 7.190e-6
-        elseif volume == 10
+        elseif diameter == 10
             rate = 2.216e-5
         end
     end
@@ -15,16 +15,16 @@ function dummy_emergent_growth(growth_a::Real, growth_b::Real, volume::Real)
 end
 
 function dummy_emergent_predation_rate(
-    predation_rate_a::Real, predation_rate_b::Real, volume::Real
+    predation_rate_a::Real, predation_rate_b::Real, diameter::Real
 )
     rate = 0
     if predation_rate_a == 0
-        return rate = 0  # Early return if volume_a is zero
+        return rate = 0  # Early return if diameter_a is zero
     else
-        # Set rate based on the value of volume
-        if volume == 10
+        # Set rate based on the value of diameter
+        if diameter == 10
             rate = 8.86e-5
-        elseif volume == 100
+        elseif diameter == 100
             rate = 4.88e-5
         end
     end
@@ -33,15 +33,15 @@ function dummy_emergent_predation_rate(
 end
 
 function dummy_emergent_nitrogen_half_saturation(
-    nitrogen_half_saturation_a::Real, nitrogen_half_saturation_b::Real, volume::Real
+    nitrogen_half_saturation_a::Real, nitrogen_half_saturation_b::Real, diameter::Real
 )
     if nitrogen_half_saturation_a == 0
-        return rate = 0  # Early return if volume_a is zero
+        return rate = 0  # Early return if diameter_a is zero
     else
-        # Set rate based on the value of volume
-        if volume == 1
+        # Set rate based on the value of diameter
+        if diameter == 1
             rate = 6.73e-3
-        elseif volume == 10
+        elseif diameter == 10
             rate = 0.12
         end
     end
@@ -50,12 +50,12 @@ function dummy_emergent_nitrogen_half_saturation(
 end
 
 function dummy_emergent_palat(prey_data, predator_data)
-    prey_volume = prey_data["volumes"]
-    predator_volume = predator_data["volumes"]
+    prey_diameter = prey_data["diameters"]
+    predator_diameter = predator_data["diameters"]
     optimum_predator_prey_ratio = predator_data["optimum_predator_prey_ratio"]
     protection = prey_data["protection"]
 
-    ratio = predator_volume / prey_volume
+    ratio = predator_diameter / prey_diameter
 
     if optimum_predator_prey_ratio == 0
         palat = 0.0
