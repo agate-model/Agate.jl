701095dc8d257f0d4d76a9fff3cdd121dcb22ffd
radka-j <r.jersakova@gmail.com>
2025-01-10 11:36:47 +0000

    docs navigation test

--- docs/make.jl
+++ docs/make.jl
@@ -3,12 +3,12 @@ using Documenter, Agate
 
 About = "Introduction" => "index.md"
 
-DevDocs = ["Documentation of Agate.jl internals" => ["devdocs/growth.md"]]
+# DevDocs = ["Documentation of Agate.jl internals" => ["devdocs/growth.md"]]
 
-PAGES = [About, DevDocs]
+PAGES = [About]
 
 makedocs(;
-    sitename="Agate.jl Documentation",
+    sitename="Agate.jl",
     format=Documenter.HTML(; prettyurls=get(ENV, "CI", nothing) == "true"),
 )
 
--- docs/src/index.md
+++ docs/src/index.md
@@ -1,3 +1,17 @@
-# Agate.jl
+# Agate.jl Documentation
 
 A Julia library to build flexible and composable aquatic ecosystems.
+
+```@contents
+```
+
+## Functions
+
+```@docs
+func(x)
+```
+
+## Index
+
+```@index
+```
