35e6cfc3523c64d01d53e00df82e13bdc6c7f461
nanophyto <coralscaper@gmail.com>
2025-01-10 17:36:11 +0000

    update test parameters (broken)

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,8 +3,8 @@ using Test
 
 # modules
 #include("test_biogeochemistry.jl")
-include("test_constructor.jl")
-#include("test_parameters.jl")
+#include("test_constructor.jl")
+include("test_parameters.jl")
 
 # integration tests
-#include("test_box_model.jl")
+include("test_box_model.jl")
--- test/test_parameters.jl
+++ test/test_parameters.jl
@@ -2,8 +2,10 @@ using Agate
 
 using Oceananigans.Units
 
-# TODO: this will eventually be replaced with the actual emergent functions
-include(joinpath("..", "examples", "emergent_4P", "emergent_functions.jl"))
+include(joinpath("..", "src", "Library", "Library.jl"))
+
+using .Library.Allometry
+using .Library.Predation
 
 @testset "Models.Parameters" begin
     @testset "create_allometric_parameters" begin
@@ -12,18 +14,18 @@ include(joinpath("..", "examples", "emergent_4P", "emergent_functions.jl"))
         defined_parameters = Dict(
             "P" => Dict(
                 "n" => 2,
-                "volumes" => Dict(
-                    "min_volume" => 1,
-                    "max_volume" => 10,
+                "diameters" => Dict(
+                    "min_diameter" => 2,
+                    "max_diameter" => 10,
                     "splitting" => "log_splitting",
                 ),
                 "allometry" => Dict(
-                    "maximum_growth_rate" => Dict("a" => 1, "b" => 1),
-                    "nitrogen_half_saturation" => Dict("a" => 1, "b" => 1),
+                    "maximum_growth_rate" => Dict("a" => 2 / 86400, "b" => -0.15),
+                    "nitrogen_half_saturation" => Dict("a" => 0.17, "b" => 0.27),
                     "maximum_predation_rate" => Dict("a" => 0, "b" => 0),
                 ),
                 "palatability" =>
-                    Dict("optimum_predator_prey_ratio" => 0, "protection" => 0),
+                    Dict("optimum_predator_prey_ratio" => 0, "protection" => 0, "specificity" => 0),
                 "assimilation_efficiency" => Dict(
                     "can_be_eaten" => 1,
                     "can_eat" => 0,
@@ -36,18 +38,18 @@ include(joinpath("..", "examples", "emergent_4P", "emergent_functions.jl"))
             ),
             "Z" => Dict(
                 "n" => 2,
-                "volumes" => Dict(
-                    "min_volume" => 10,
-                    "max_volume" => 100,
+                "diameters" => Dict(
+                    "min_diameter" => 20,
+                    "max_diameter" => 100,
                     "splitting" => "linear_splitting",
                 ),
                 "allometry" => Dict(
                     "maximum_growth_rate" => Dict("a" => 0, "b" => 0),
                     "nitrogen_half_saturation" => Dict("a" => 0, "b" => 0),
-                    "maximum_predation_rate" => Dict("a" => 1, "b" => 1),
+                    "maximum_predation_rate" => Dict("a" => 30.84 / 86400, "b" => -0.16),
                 ),
                 "palatability" =>
-                    Dict("optimum_predator_prey_ratio" => 10, "protection" => 1),
+                    Dict("optimum_predator_prey_ratio" => 10, "protection" => 1, "specificity" => 0.3),
                 "assimilation_efficiency" => Dict(
                     "can_be_eaten" => 0,
                     "can_eat" => 1,
@@ -62,25 +64,28 @@ include(joinpath("..", "examples", "emergent_4P", "emergent_functions.jl"))
 
         emergent_parameters = compute_allometric_parameters(defined_parameters)
 
-        # compare against hand computer `parameters` in examples
+        # compare against hand computed `parameters` in examples
         include(joinpath("..", "examples", "N2P2ZD", "tracers.jl"))
 
-        plankton_order = ["P1", "P2", "Z1", "Z2"]
+        plankton_order = [ "P1",  "P2",  "Z1", "Z2"]
 
         for (key, emerge_params) in emergent_parameters
             # start with arrays of values
-            if !(key ∈ ["assimilation_efficiency_matrix", "palatability_matrix", "volumes"])
+            if !(key in ["assimilation_efficiency_matrix", "palatability_matrix", "diameters"])
                 @test all(
-                    parameters[Symbol(key)] .== [emerge_params[p] for p in plankton_order]
-                )
+                    isapprox.(parameters[Symbol(key)], [emerge_params[p] for p in plankton_order], rtol=0.01)) || 
+                    println("Test failed for parameter: $(key). Expected: $(parameters[Symbol(key)]), Got: $([emerge_params[p] for p in plankton_order])")
                 # matrices of values -> compare row at a time
-            elseif !(key == "volumes")
+            elseif !(key == "diameters")
                 for (i, p) in enumerate(plankton_order)
                     emergent_row = emerge_params[p, :]
                     true_row = parameters[Symbol(replace(key, "_matrix" => ""))][i, :]
-                    @test all(true_row .== [emergent_row[p] for p in plankton_order])
+                    @test all(
+                        isapprox.(true_row, [emergent_row[p] for p in plankton_order], rtol=0.01)) || 
+                        println("Test failed for parameter: $(key), row: $(i). Expected: $(true_row), Got: $([emergent_row[p] for p in plankton_order])")
                 end
             end
         end
+
     end
 end
