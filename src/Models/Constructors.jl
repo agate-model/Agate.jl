580b5ed77b9e70871194e09b1ee3a6bdbdca0019
Joost de Vries <36844534+nanophyto@users.noreply.github.com>
2025-01-13 12:03:17 +0000

    Update src/Models/Constructors.jl
    Co-authored-by: Radka Jersakova <r.jersakova@gmail.com>

--- src/Models/Constructors.jl
+++ src/Models/Constructors.jl
@@ -60,7 +60,7 @@ function construct_size_structured_NPZD(;
         "diameters" =>
             Dict("min_diameter" => 2, "max_diameter" => 10, "splitting" => "log_splitting"),
         "allometry" => Dict(
-            "maximum_growth_rate" => Dict("a" => 2 / 86400, "b" => -0.15),
+            "maximum_growth_rate" => Dict("a" => 2 / day, "b" => -0.15),
             "nitrogen_half_saturation" => Dict("a" => 0.17, "b" => 0.27),
         ),
         "linear_mortality" => 8e-7 / second,
