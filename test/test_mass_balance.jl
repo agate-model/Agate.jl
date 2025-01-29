6bfb765739f10e3dd686c0bd4a3264d85f55f5b7
nanophyto <coralscaper@gmail.com>
2025-01-29 15:59:40 +0000

    add atol and check drift over time

--- test/test_mass_balance.jl
+++ test/test_mass_balance.jl
@@ -40,13 +40,13 @@ const year = years = 365day
     end
     @testset "thunder_egg_1 box model" begin
         model = construct_thunder_egg_1()
-
+    
         bgc_model = Biogeochemistry(
             model(); light_attenuation=FunctionFieldPAR(; grid=BoxModelGrid())
         )
         box_model = BoxModel(; biogeochemistry=bgc_model)
         set!(box_model; DIN=7, PO4=3, P1=0.01, P2=0.01, Z1=0.05, Z2=0.05, DOC=0, POC=0.0)
-
+    
         function estimate_carbon_mass(box_model)
             return box_model.fields.DIC.data[1, 1, 1] +
                    box_model.fields.P1.data[1, 1, 1] +
@@ -56,14 +56,24 @@ const year = years = 365day
                    box_model.fields.POC.data[1, 1, 1] +
                    box_model.fields.DOC.data[1, 1, 1]
         end
-
+    
+        carbon_mass_records = Float64[]
         initial_carbon_mass = estimate_carbon_mass(box_model)
-
+        push!(carbon_mass_records, initial_carbon_mass)
+    
         for n in 1:1000
             time_step!(box_model, 1.0)
+            if n % 100 == 0
+                push!(carbon_mass_records, estimate_carbon_mass(box_model))
+            end
         end
-
+    
         final_carbon_mass = estimate_carbon_mass(box_model)
-        @test isapprox(initial_carbon_mass, final_carbon_mass)
+        push!(carbon_mass_records, final_carbon_mass)
+    
+        atol = 1e10
+        @test isapprox(initial_carbon_mass, final_carbon_mass, atol=atol)
+        @test all(diff(carbon_mass_records) .≥ -atol)  # Allow only negligible decreases
     end
+    
 end
