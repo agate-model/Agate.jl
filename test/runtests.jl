e3a6270b915be9e3c31844be728909f1819b6ef3
nanophyto <coralscaper@gmail.com>
2025-01-29 12:32:11 +0000

    disable other tests to save half a second during debugging

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,13 @@
 using Agate
 using Test
 
-# modules
-include("test_biogeochemistry.jl")
-include("test_constructor.jl")
-include("test_parameters.jl")
+# # modules
+# include("test_biogeochemistry.jl")
+# include("test_constructor.jl")
+# include("test_parameters.jl")
 
-# integration tests
-include("test_box_model.jl")
+# # integration tests
+# include("test_box_model.jl")
 
 # model checks
 include("test_mass_balance.jl")
