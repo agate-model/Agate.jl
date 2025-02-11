03e954a9ff865b4d1cae9c37deebab5a72d9eb97
radka-j <r.jersakova@gmail.com>
2025-02-11 16:46:45 +0000

    update test

--- test/test_constructor.jl
+++ test/test_constructor.jl
@@ -3,8 +3,6 @@ using NamedArrays
 using Agate.Models.Tracers
 
 @testset "Models.Constructor" begin
-
-    # N2P2ZD model constructed from emergent parameters
     N2P2ZD_constructed = Agate.Constructors.NPZD_size_structured.construct()
 
     # declare constants reused in all tests
@@ -17,10 +15,12 @@ using Agate.Models.Tracers
     PAR = 100
 
     @testset "N2P2ZD model" begin
+
         # N2P2ZD model defined using low level syntax
         include(joinpath("..", "examples", "N2P2ZD", "tracers.jl"))
         model = N2P2ZD()
 
+        # N2P2ZD model constructed from emergent parameters
         model_constructed = Agate.Constructors.NPZD_size_structured.instantiate(
             N2P2ZD_constructed
         )
@@ -84,52 +84,53 @@ using Agate.Models.Tracers
         )
     end
 
-    # @testset "Diameters passed as an array" begin
+    @testset "Diameters passed as an array" begin
 
-    #     # the default type doe not allow for 1 phytoplankton
-    #     # @test_throws ArgumentError Agate.Constructors.NPZD_size_structured.instantiate(
-    #     #     N2P2ZD_constructed; phyto_diameters=[1]
-    #     #     )
+        # the default type defined at the top has 2 phyto so expect 2 diameters
+        @test_throws ArgumentError Agate.Constructors.NPZD_size_structured.instantiate(
+            N2P2ZD_constructed; phyto_diameters=[1]
+        )
 
-    #     # diameters can be passed as an array of values rather than a dictionary
-    #     # this is useful in the case where we want 1 phytoplankton with a given diameter
-    #     # it could also be used to fix the diameters of multiple phytoplankton in the model
-    #     NP2ZD = Agate.Constructors.NPZD_size_structured.construct(; n_phyto=1)
-    #     model = Agate.Constructors.NPZD_size_structured.instantiate(
-    #         N2P2ZD; phyto_diameters=[2]
-    #         )
+        # the default type defined at the top has 2 phyto so expect 2 diameters
+        @test_throws ArgumentError Agate.Constructors.NPZD_size_structured.instantiate(
+            N2P2ZD_constructed; phyto_diameters=[1, 2, 3]
+        )
 
-    #     # this model only has 1 phyto, 2 zoo tracers (unlike other tests here)
-    #     @test !iszero(model(Val(:N), 0, 0, 0, 0, P1, Z1, Z2, N, D, PAR))
-    #     @test !iszero(model(Val(:D), 0, 0, 0, 0, P1, Z1, Z2, N, D, PAR))
-    #     @test !iszero(model(Val(:P1), 0, 0, 0, 0, P1, Z1, Z2, N, D, PAR))
-    #     @test !iszero(model(Val(:Z1), 0, 0, 0, 0, P1, Z1, Z2, N, D, PAR))
-    #     @test !iszero(model(Val(:Z2), 0, 0, 0, 0, P1, Z1, Z2, N, D, PAR))
+        # diameters can be passed as an array of values rather than a dictionary
+        # this is useful in the case where we want 1 phytoplankton with a given diameter
+        # it could also be used to fix the diameters of multiple phytoplankton in the model
+        NP2ZD = Agate.Constructors.NPZD_size_structured.construct(; n_phyto=1)
+        model = Agate.Constructors.NPZD_size_structured.instantiate(
+            NP2ZD; phyto_diameters=[2]
+        )
 
-    #     # the default type defined at the top has 2 phyto so expect 2 diameters
-    #     @test_throws ArgumentError Agate.Constructors.NPZD_size_structured.instantiate(
-    #         N2P2ZD_constructed; phyto_diameters=[1, 2, 3]
-    #         )
+        # this model only has 1 phyto, 2 zoo tracers (unlike other tests here)
+        @test !iszero(model(Val(:N), 0, 0, 0, 0, P1, Z1, Z2, N, D, PAR))
+        @test !iszero(model(Val(:D), 0, 0, 0, 0, P1, Z1, Z2, N, D, PAR))
+        @test !iszero(model(Val(:P1), 0, 0, 0, 0, P1, Z1, Z2, N, D, PAR))
+        @test !iszero(model(Val(:Z1), 0, 0, 0, 0, P1, Z1, Z2, N, D, PAR))
+        @test !iszero(model(Val(:Z2), 0, 0, 0, 0, P1, Z1, Z2, N, D, PAR))
+    end
 
-    # end
+    @testset "Alternative instantiation" begin
 
-    # @testset "Alternative instantiation" begin
+        # N2P2ZD model constructed with user-defined functions (geider growth)
+        N2P2ZD_geider = Agate.Constructors.NPZD_size_structured.construct(;
+            phyto_args=Agate.Constructors.NPZD_size_structured.DEFAULT_PHYTO_GEIDER_ARGS,
+            nutrient_dynamics=nutrients_geider_light,
+            phyto_dynamics=phytoplankton_growth_single_nutrient_geider_light,
+        )
 
-    #     # N2P2ZD model constructed with user-defined functions (geider growth)
-    #     N2P2ZD_geider = Agate.Constructors.NPZD_size_structured.construct(;
-    #         phyto_args=Agate.Constructors.NPZD_size_structured.DEFAULT_PHYTO_GEIDER_ARGS,
-    #         nutrient_dynamics=nutrients_geider_light,
-    #         phyto_dynamics=phytoplankton_growth_single_nutrient_geider_light,
-    #     )
+        model_geider = Agate.Constructors.NPZD_size_structured.instantiate(
+            N2P2ZD_geider;
+            phyto_args=Agate.Constructors.NPZD_size_structured.DEFAULT_PHYTO_GEIDER_ARGS,
+        )
 
-    #     model_geider = Agate.Constructors.NPZD_size_structured.instantiate(N2P2ZD_geider)
-
-    #     @test !iszero(model_geider(Val(:N), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR))
-    #     @test !iszero(model_geider(Val(:D), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR))
-    #     @test !iszero(model_geider(Val(:P1), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR))
-    #     @test !iszero(model_geider(Val(:P2), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR))
-    #     @test !iszero(model_geider(Val(:Z1), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR))
-    #     @test !iszero(model_geider(Val(:Z2), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR))
-    # end
-
+        @test !iszero(model_geider(Val(:N), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR))
+        @test !iszero(model_geider(Val(:D), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR))
+        @test !iszero(model_geider(Val(:P1), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR))
+        @test !iszero(model_geider(Val(:P2), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR))
+        @test !iszero(model_geider(Val(:Z1), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR))
+        @test !iszero(model_geider(Val(:Z2), 0, 0, 0, 0, P1, P2, Z1, Z2, N, D, PAR))
+    end
 end
