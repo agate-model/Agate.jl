48d36a2d949bff406b06736f8a7c610d7e96b0fe
nanophyto <coralscaper@gmail.com>
2025-02-14 10:26:09 +0000

    add tests for predation functions

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,16 @@
 using Agate
 using Test
 
-# # modules
-# include("test_biogeochemistry.jl")
-# include("test_constructor.jl")
-# include("test_parameters.jl")
+# modules
+include("test_biogeochemistry.jl")
+include("test_constructor.jl")
+include("test_parameters.jl")
 
-# # integration tests
-# include("test_box_model.jl")
+# library
+include("test_library_predation.jl")
+
+# integration tests
+include("test_box_model.jl")
 
 # model checks
-include("test_mass_balance.jl")
+include("test_mass_balance.jl") 
