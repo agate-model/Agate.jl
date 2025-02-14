7f96e4fb32ddb7a23582cf34b8eeeec0b4d682a5
nanophyto <coralscaper@gmail.com>
2025-02-14 16:04:06 +0000

    remove broken tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,10 +6,6 @@ include("test_biogeochemistry.jl")
 include("test_constructor.jl")
 include("test_parameters.jl")
 
-# library
-include("test_library_predation.jl")
-include("test_library_mortality.jl")
-
 # integration tests
 include("test_box_model.jl")
 
