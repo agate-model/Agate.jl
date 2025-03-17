b780f6bf9abcdea4d9f55ea6536f680c93a5db60
nanophyto <coralscaper@gmail.com>
2025-03-17 09:31:47 +0000

    tidy example and add note

--- examples/1D_column.jl
+++ examples/1D_column.jl
@@ -1,5 +1,9 @@
 # # [1D water column example] (@id 1D_column_example)
 
+# !!! info
+#     This example uses [Oceananigans.jl](https://clima.github.io/OceananigansDocumentation/stable/) and [OceanBioME.jl](https://oceanbiome.github.io/OceanBioME.jl/stable/).
+#     We recommend familiarizing yourself with their user interface if you intend to make changes to the physical model setup.
+
 # In this example we run a default `Agate.jl-NiPiZD` model inside a simple 1D water column model.
 # The physical model setup is based on an example provided in the OceanBioME.jl documentation and represents an idealized 200m deep North Atlantic time series.
 
@@ -63,14 +67,14 @@ end
 t_range = 0.0:days:(365.0 * days)  # Time range from 0 to 365 days 
 z_range = -200.0:10.0:0.0  # Depth range from -200m to 0m 
 x, y, z = 0.0, 0.0, 0.0
-κₜ_values = [log(diffusivity(x, y, z, t)) for t in t_range, z in z_range]
+κₜ_values = [diffusivity(x, y, z, t) for t in t_range, z in z_range]
 PAR_values = [log(cyclical_PAR(x, y, z, t)) for t in t_range, z in z_range]
 
 fig_forcing = Figure(; resolution=(1000, 800))
-ax1 = Axis(fig_forcing[1, 1]; xlabel="Time (days)", ylabel="PAR0", title="log(irradiance)")
+ax1 = Axis(fig_forcing[1, 1]; xlabel="Time (days)", ylabel="Depth (m)", title="log(irradiance)")
 CairoMakie.heatmap!(ax1, t_range ./ days, z_range, PAR_values; colormap=:viridis)
 
-ax2 = Axis(fig_forcing[2, 1]; xlabel="Time (days)", ylabel="Depth (m)", title="log(diffusivity)")
+ax2 = Axis(fig_forcing[2, 1]; xlabel="Time (days)", ylabel="Depth (m)", title="diffusivity")
 CairoMakie.heatmap!(ax2, t_range ./ days, z_range, κₜ_values; colormap=:viridis)
 
 fig_forcing
@@ -85,11 +89,9 @@ bgc_model = Biogeochemistry(
 )
 nothing #hide
 
-clock = Clock(; time=0.0)
-
 full_model = NonhydrostaticModel(;
     grid,
-    clock,
+    clock = Clock(; time=0.0),
     closure=ScalarDiffusivity(; ν=diffusivity, κ=diffusivity),
     biogeochemistry=bgc_model,
 )
@@ -102,7 +104,7 @@ set!(full_model; N=7.0, P1=0.01, P2=0.01, Z1=0.05, Z2=0.05, D=0.0)
 # ## Simulation
 filename = "N2P2ZD_column.jld2"
 
-simulation = Simulation(full_model; Δt=20minutes, stop_time=1years)
+simulation = Simulation(full_model; Δt=20minutes, stop_time=1day)
 
 simulation.output_writers[:profiles] = JLD2OutputWriter(
     full_model,
@@ -113,84 +115,46 @@ simulation.output_writers[:profiles] = JLD2OutputWriter(
 )
 
 run!(simulation)
-
 nothing #hide
 
 # ## Plotting
-using CairoMakie
 
 #Load time series data
 timeseries = NamedTuple{keys(full_model.tracers)}(
     FieldTimeSeries(filename, "$field") for field in keys(full_model.tracers)
 )
-nothing #hide
 
 timeseries_keys = keys(timeseries)
 nothing #hide
 
+#Filter keys for P, Z, N, and D fields
 P_keys = filter(k -> startswith(string(k), "P"), timeseries_keys)
 Z_keys = filter(k -> startswith(string(k), "Z"), timeseries_keys)
+N_key = :N
+D_key = :D
 
-fig = Figure(; size=(1400, 400 * (length(P_keys) + length(Z_keys) + 2)), fontsize=20)
+#Combine all keys into a single list for iteration
+all_keys = [P_keys..., Z_keys..., N_key, D_key]
 
+#Create figure with appropriate size
+fig = Figure(; size=(250*length(all_keys), 400 * length(all_keys)), fontsize=20)
+
+#Axis configuration
 axis_kwargs = (xlabel="Time (days)", ylabel="z (m)", limits=((0, nothing), (-200, 0)))
 
-for (i, key) in enumerate(P_keys)
+#Plot all fields
+for (i, key) in enumerate(all_keys)
     x, y, z = nodes(timeseries[key])
     z = collect(z)  # Ensure z is a vector
     times = collect(timeseries[key].times / days)  # Convert times to a vector
 
-    axP = Axis(fig[i, 1]; title="$(key) concentration (mmol N / m³)", axis_kwargs...)
-    hmP = heatmap!(
-        axP, times, z, Float32.(interior(timeseries[key], 1, 1, :, :)'); colormap=:viridis
+    #Create axis and heatmap
+    ax = Axis(fig[i, 1]; title="$(key) concentration (mmol N / m³)", axis_kwargs...)
+    hm = heatmap!(
+        ax, times, z, Float32.(interior(timeseries[key], 1, 1, :, :)'); colormap=:viridis
     )
-    Colorbar(fig[i, 2], hmP)
+    Colorbar(fig[i, 2], hm)
 end
-
-#Plot Zooplankton data
-for (i, key) in enumerate(Z_keys)
-    x, y, z = nodes(timeseries[key])
-    z = collect(z)
-    times = collect(timeseries[key].times / days)
-
-    axZ = Axis(
-        fig[i + length(P_keys), 1];
-        title="$(key) concentration (mmol N / m³)",
-        axis_kwargs...,
-    )
-    hmZ = heatmap!(
-        axZ, times, z, Float32.(interior(timeseries[key], 1, 1, :, :)'); colormap=:viridis
-    )
-    Colorbar(fig[i + length(P_keys), 2], hmZ)
-end
-
-#Plot Nutrient data
-times = collect(timeseries[:N].times / days)
-x, y, z = nodes(timeseries[:N])
-z = collect(z)
-
-axD = Axis(
-    fig[length(P_keys) + length(Z_keys) + 1, 1];
-    title="Nutrient concentration (mmol N / m³)",
-    axis_kwargs...,
-)
-dataD = Float32.(interior(timeseries[:N], 1, 1, :, :)')  # Convert data to Float32
-hmD = heatmap!(axD, times, z, dataD; colormap=:viridis)
-Colorbar(fig[length(P_keys) + length(Z_keys) + 1, 2], hmD)
-
-#Plot Detritus data
-times = collect(timeseries[:D].times / days)
-x, y, z = nodes(timeseries[:D])
-z = collect(z)
-
-axD = Axis(
-    fig[length(P_keys) + length(Z_keys) + 2, 1];
-    title="Detritus concentration (mmol N / m³)",
-    axis_kwargs...,
-)
-dataD = Float32.(interior(timeseries[:D], 1, 1, :, :)')  # Convert data to Float32
-hmD = heatmap!(axD, times, z, dataD; colormap=:viridis)
-Colorbar(fig[length(P_keys) + length(Z_keys) + 2, 2], hmD)
 
 #Save figure
 save("N2P2ZD_column.png", fig)
