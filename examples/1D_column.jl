126d99e7ee9931fbcc7900fa143600359842684e
nanophyto <coralscaper@gmail.com>
2025-03-18 17:22:33 +0000

    final fixes

--- examples/1D_column.jl
+++ examples/1D_column.jl
@@ -4,7 +4,7 @@
 #     This example uses [Oceananigans.jl](https://clima.github.io/OceananigansDocumentation/stable/) and [OceanBioME.jl](https://oceanbiome.github.io/OceanBioME.jl/stable/).
 #     We recommend familiarizing yourself with their user interface if you intend to make changes to the physical model setup.
 
-# In this example we run a default `Agate.jl-NiPiZD` model inside a simple 1D water column model.
+# In this example we run a default [Agate.jl-NiPiZD](@ref NiPiZD) model inside a simple 1D water column model.
 # The physical model setup is based on an example provided in the OceanBioME.jl documentation and represents an idealized 200m deep North Atlantic time series.
 
 # ## Loading dependencies
@@ -68,10 +68,10 @@ t_range = 0.0:days:(365.0 * days)  # Time range from 0 to 365 days
 z_range = -200.0:10.0:0.0  # Depth range from -200m to 0m 
 x, y, z = 0.0, 0.0, 0.0
 κₜ_values = [diffusivity(x, y, z, t) for t in t_range, z in z_range]
-PAR_values = [log(cyclical_PAR(x, y, z, t)) for t in t_range, z in z_range]
+PAR_values = [cyclical_PAR(x, y, z, t) for t in t_range, z in z_range]
 
 fig_forcing = Figure(; resolution=(1000, 800))
-ax1 = Axis(fig_forcing[1, 1]; xlabel="Time (days)", ylabel="Depth (m)", title="log(irradiance)")
+ax1 = Axis(fig_forcing[1, 1]; xlabel="Time (days)", ylabel="Depth (m)", title="irradiance")
 CairoMakie.heatmap!(ax1, t_range ./ days, z_range, PAR_values; colormap=:viridis)
 
 ax2 = Axis(fig_forcing[2, 1]; xlabel="Time (days)", ylabel="Depth (m)", title="diffusivity")
@@ -91,7 +91,7 @@ nothing #hide
 
 full_model = NonhydrostaticModel(;
     grid,
-    clock = Clock(; time=0.0),
+    clock=Clock(; time=0.0),
     closure=ScalarDiffusivity(; ν=diffusivity, κ=diffusivity),
     biogeochemistry=bgc_model,
 )
@@ -99,12 +99,12 @@ nothing #hide
 
 # ## Initial conditions
 
-set!(full_model; N=7.0, P1=0.01, P2=0.01, Z1=0.05, Z2=0.05, D=0.0)
+set!(full_model; N=7.0, P1=0.01, P2=0.01, Z1=0.05, Z2=0.05, D=0.0) # mmol N / m³
 
 # ## Simulation
 filename = "N2P2ZD_column.jld2"
 
-simulation = Simulation(full_model; Δt=20minutes, stop_time=1day)
+simulation = Simulation(full_model; Δt=20minutes, stop_time=1year)
 
 simulation.output_writers[:profiles] = JLD2OutputWriter(
     full_model,
@@ -137,7 +137,7 @@ D_key = :D
 all_keys = [P_keys..., Z_keys..., N_key, D_key]
 
 #Create figure with appropriate size
-fig = Figure(; size=(250*length(all_keys), 400 * length(all_keys)), fontsize=20)
+fig = Figure(; size=(250 * length(all_keys), 400 * length(all_keys)), fontsize=20)
 
 #Axis configuration
 axis_kwargs = (xlabel="Time (days)", ylabel="z (m)", limits=((0, nothing), (-200, 0)))
