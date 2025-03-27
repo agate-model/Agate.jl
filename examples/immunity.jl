7f0e3ce625f9d91fb01ba695fa3556fa2bd8f0d5
nanophyto <coralscaper@gmail.com>
2025-03-27 14:47:38 +0000

    start adding colors to graphs

--- examples/immunity.jl
+++ examples/immunity.jl
@@ -7,10 +7,9 @@ using OceanBioME
 using OceanBioME: Biogeochemistry
 using Oceananigans
 using Oceananigans.Units
-using Graphs, GraphPlot
 
 const year = years = 365day
-
+using Graphs, GraphPlot, Colors, ColorSchemes
 
 # Construct the ecosystem model
 N2P2ZD = NiPiZD.construct()
@@ -31,24 +30,56 @@ nothing #hide
 #     However, Graphs.jl follows the graph theory convention where the sources (prey) = rows, and the targets (predators) = columns.
 #     To plot the graphs, and to add nodes, we thus need to transpose (`'`) the matrices before plotting and before instantiating the new model.
 
+# Function to map values to Viridis colors
+function value_to_color(value, min_val, max_val)
+    norm_value = (value - min_val) / (max_val - min_val + 1e-5)  # Avoid division by zero
+    return get(ColorSchemes.viridis, norm_value)  # Use Viridis color scheme
+end
 
-g = SimpleDiGraph(palat_matrix.array') # ' to transpose
-gplot(g, nodelabel=labels, layout=circular_layout)
+function plot_graph_with_colored_edges(g, matrix, labels)
+    edge_list = collect(edges(g))  # Extract edges as SimpleEdge{Int}
+    edge_values = [matrix[e.dst, e.src] for e in edge_list]  # Extract edge weights from the TRANSPOSED matrix
+    min_val, max_val = minimum(edge_values), maximum(edge_values)
+    edge_colors = [value_to_color(v, min_val, max_val) for v in edge_values]    
+    gplot(g,
+        nodelabel=labels,  # Add node labels
+        edgestrokec=edge_colors,  # Color edges based on weights
+        edgelabel=edge_values, 
+        layout=circular_layout
+    )
+end
 
-# Add predation of Z2 on Z1
+# ## Plot default palatability matrices
+g_palat = SimpleDiGraph(palat_matrix.array')
+plot_graph_with_colored_edges(g_palat, palat_matrix.array, labels)
+
+# ## Update palatability matrix
+
+# Add new edges:
 z1, z2 = findfirst(isequal("Z1"), labels), findfirst(isequal("Z2"), labels)
-add_edge!(g, z1, z2)
+add_edge!(g_palat, z1, z2)
+palat_matrix.array .= Graphs.adjacency_matrix(g_palat)' # ' to transpose
 nothing #hide
 
-# Plot the new graph
-gplot(g, nodelabel=labels, layout=circular_layout)
+# Plot:
+plot_graph_with_colored_edges(g_palat, palat_matrix.array, labels)
 
-# Update palat_matrix with the new adjacency matrix
-palat_matrix.array .= Graphs.adjacency_matrix(g)' # ' to transpose
+
+#  ## Update assimilation matrix
+# We also need to update the assimilation matrix. 
+# We set the value for the new link to be the default 0.32.
+
+g_assim = SimpleDiGraph(assim_matrix.array')
+add_edge!(g_assim, z1, z2)
+assim_matrix.array .= Graphs.adjacency_matrix(g_palat)' # ' to transpose
 nothing #hide
 
-# Instantiate the updated model
-new_model = NiPiZD.instantiate(N2P2ZD; palatability_matrix=palat_matrix)
+
+
+# ## Instantiate models
+new_model = NiPiZD.instantiate(N2P2ZD; 
+    palatability_matrix=palat_matrix,
+    assimilation_efficiency_matrix=assim_matrix)
 
 
 bgc_model_default = Biogeochemistry(
@@ -100,30 +131,28 @@ timeseries_new = run_simulation!(
 )
 
 
-# ==================================================
-# Plotting
-# ==================================================
-using Plots
+# # Plotting
+# using Plots
 
-fields = [:P1, :P2, :Z1, :Z2, :D, :N]
-titles = [
-    "Phytoplankton 1",
-    "Phytoplankton 2",
-    "Zooplankton 1",
-    "Zooplankton 2",
-    "Detritus",
-    "Nutrient",
-]
+# fields = [:P1, :P2, :Z1, :Z2, :D, :N]
+# titles = [
+#     "Phytoplankton 1",
+#     "Phytoplankton 2",
+#     "Zooplankton 1",
+#     "Zooplankton 2",
+#     "Detritus",
+#     "Nutrient",
+# ]
 
-plots = [
-    plot(; title=titles[i], xlabel="Time (days)", ylabel=string(fields[i])) for
-    i in 1:length(fields)
-]
+# plots = [
+#     plot(; title=titles[i], xlabel="Time (days)", ylabel=string(fields[i])) for
+#     i in 1:length(fields)
+# ]
 
-for (i, field) in enumerate(fields)
-    plot!(plots[i], timeseries_default[field]; label="immunity", color=:blue)
-    plot!(plots[i], timeseries_new[field]; label="no immunity", color=:green)
-end
+# for (i, field) in enumerate(fields)
+#     plot!(plots[i], timeseries_default[field]; label="immunity", color=:blue)
+#     plot!(plots[i], timeseries_new[field]; label="no immunity", color=:green)
+# end
 
-p = plot(plots...; layout=(2, 3), size=(900, 600))
-p
+# p = plot(plots...; layout=(2, 3), size=(900, 600))
+# p
