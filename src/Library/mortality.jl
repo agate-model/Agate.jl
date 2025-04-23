c2804157c50035649209da3300bd4cad37d391e2
nanophyto <coralscaper@gmail.com>
2025-04-23 15:45:41 +0100

    replace short names with long names

--- src/Library/mortality.jl
+++ src/Library/mortality.jl
@@ -3,46 +3,46 @@ module Mortality
 export linear_loss, quadratic_loss
 
 """
-    linear_loss(P, l)
+    linear_loss(plankton_concentration, mortality)
 
 Linear mortality rate.
 
 !!! formulation
-    l * P
+    ``l`` * ``P``
 
     where:
-    - P = plankton concentration
-    - l = mortality rate
+    - ``P`` = plankton concentration
+    - ``l`` = mortality rate
 
 In this formulation mortality is constant, and can be interpreted as
 a "closure term" for low density predation and and other death terms.
 
 # Arguments
-- `P`: plankton concentration
-- `l`: mortality rate
+- `plankton_concentration`: plankton concentration
+- `mortality`: mortality rate
 """
-linear_loss(P, l) = l * P
+linear_loss(plankton_concentration, mortality) = mortality * plankton_concentration
 
 """
-    quadratic_loss(P, l)
+    quadratic_loss(plankton_concentration, mortality)
 
 Quadratic mortality rate.
 
 !!! formulation
 
-    l * P^2
+    ``l`` * ``P``^2
 
     where:
-    - P = plankton concentration
-    - l = mortality rate
+    - ``P`` = plankton concentration
+    - ``l`` = mortality rate
 
 In this formulation mortality increases exponentially with plankton biomass
 and is often interpreted to represent viral processes and non-represented density-dependent predation effects.
 
 # Arguments
-- `P`: plankton concentration
-- `l`: mortality rate
+- `plankton_concentration`: plankton concentration
+- `mortality`: mortality rate
 """
-quadratic_loss(P, l) = l * P^2
+quadratic_loss(plankton_concentration, mortality) = mortality * plankton_concentration^2
 
 end # module
