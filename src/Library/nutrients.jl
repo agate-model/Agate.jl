34aee2d66526beaf7e00bb9a134d76a06223a99e
nanophyto <coralscaper@gmail.com>
2025-04-23 16:23:41 +0100

    add formulations

--- src/Library/nutrients.jl
+++ src/Library/nutrients.jl
@@ -7,24 +7,38 @@ module Nutrients
 export monod_limitation, liebig_minimum
 
 "
-    R / (kᵣ + R)
+    monod_limitation(nutrient_concentration, nutrient_half_saturation)
 
 Monod formulation of nutrient limitation, which is based on Michaelis-Menten enzyme kinetics.
 
+!!! formulation
+    ``R`` / (``kᵣ`` + ``R``)
+
+    where:
+    - ``R`` = nutrient concentration (e.g. N, P, Si)
+    - ``kᵣ`` = nutrient half saturation constant    
+
 # Arguments
-- `R`: nutrient (e.g. N, P, Si)
-- `kᵣ`: nutrient half saturation constant
+- `nutrient_concentration`: nutrient (e.g. N, P, Si)
+- `nutrient_half_saturation`: nutrient half saturation constant
 
 Note that sometimes this formulation is also used for Predation ('Holling type 2').
 "
-function monod_limitation(R, kᵣ)
-    return R / (kᵣ + R)
+function monod_limitation(nutrient_concentration, nutrient_half_saturation)
+    return nutrient_concentration / (nutrient_half_saturation + nutrient_concentration)
 end
 
 """
     liebig_minimum(nutrient_limitations)
 
 Liebig's law of the minimum, which states that growth is limited by the scarcest (most limiting) resource.
+
+!!! formulation
+    minimum(nutrient_limitations)
+
+    where:
+    - nutrient_limitations = an array of nutrient limitation values
+        (e.g. [N, P, Si])
 
 # Arguments
 - `nutrient_limitations`: an array of nutrient limitation values
