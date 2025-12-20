4285f96e54bf4cc969692cacfc720cce2e92b3fb
nanophyto <coralscaper@gmail.com>
2025-12-20 14:48:33 +0000

    working example (!)

--- paper/GPU/column_N2P2ZD_agate.jl
+++ paper/GPU/column_N2P2ZD_agate.jl
@@ -13,61 +13,72 @@ using Oceananigans.Biogeochemistry: required_biogeochemical_tracers
 using Adapt
 using CUDA
 
-# Generate the biogeochemical model (CPU)
+# Generate the biogeochemical model
 bgc_type = NiPiZD.construct()
+
+# Create an instance of the model (CPU)
 bgc_instance = bgc_type()
 
-# IMPORTANT: query tracer names on the CPU instance (this is the type Agate attached methods to)
+# IMPORTANT: get tracer names from the CPU instance (Agate defines the method on this type)
 tracer_names = required_biogeochemical_tracers(bgc_instance)
 
-# Adapt the instance to GPU
+# Test GPU compatibility
 adapted_instance = Adapt.adapt(CuArray, bgc_instance)
-
-biogeochemistry = Biogeochemistry(
-    adapted_instance;
-    light_attenuation = FunctionFieldPAR(; grid = BoxModelGrid()),
-)
 
 const year = years = 365days
 nothing #hide
 
-@inline PAR⁰(x, y, t) = 60 * (1 - cos((t + 15days) * 2π / year)) *
-                        (1 / (1 + 0.2 * exp(-((mod(t, year) - 200days) / 50days)^2))) + 2
+@inline PAR⁰(x, y, t) =
+    60 * (1 - cos((t + 15days) * 2π / year)) *
+    (1 / (1 + 0.2 * exp(-((mod(t, year) - 200days) / 50days)^2))) + 2
+
+@inline PAR_f(x, y, z, t) = PAR⁰(x, y, t) * exp(0.2 * z)
 
 @inline H(t, t₀, t₁) = ifelse(t₀ < t < t₁, 1.0, 0.0)
 
-@inline fmld1(t) = H(t, 50days, year) *
-                   (1 / (1 + exp(-(t - 100days) / 5days))) *
-                   (1 / (1 + exp((t - 330days) / 25days)))
+@inline fmld1(t) =
+    H(t, 50days, year) *
+    (1 / (1 + exp(-(t - 100days) / 5days))) *
+    (1 / (1 + exp((t - 330days) / 25days)))
 
-@inline MLD(t) = - (10 + 340 * (1 - fmld1(year - eps(year)) * exp(-mod(t, year) / 25days) - fmld1(mod(t, year))))
+@inline MLD(t) =
+    -(10 + 340 * (1 - fmld1(year - eps(year)) * exp(-mod(t, year) / 25days) - fmld1(mod(t, year))))
 
 @inline κₜ(x, y, z, t) = 1e-2 * (1 + tanh((z - MLD(t)) / 10)) / 2 + 1e-4
 
 @inline temp(x, y, z, t) = 2.4 * cos(t * 2π / year + 50days) + 10
 
-grid = RectilinearGrid(GPU(), size = (1, 1, 50), extent = (20meters, 20meters, 200meters))
+grid = RectilinearGrid(GPU(), size=(1, 1, 50), extent=(20meters, 20meters, 200meters))
 
-clock = Clock(; time = 0.0)
+# Build light attenuation on the column grid
+light_attenuation = FunctionFieldPAR(; grid, PAR_f=PAR_f)
+
+biogeochemistry = Biogeochemistry(adapted_instance; light_attenuation)
+
+clock = Clock(; time=0.0)
 T = FunctionField{Center, Center, Center}(temp, grid; clock)
 S = ConstantField(35)
 
-# IMPORTANT: explicitly pass tracers so P1/P2/etc exist
-model = NonhydrostaticModel(; grid,
-                              clock,
-                              tracers = tracer_names,
-                              closure = ScalarDiffusivity(ν = κₜ, κ = κₜ),
-                              biogeochemistry,
-                              auxiliary_fields = (; T, S))
+model = NonhydrostaticModel(;
+    grid,
+    clock,
+    tracers = tracer_names,  # IMPORTANT: ensure P1/P2/Z1/Z2/N/D exist
+    closure = ScalarDiffusivity(ν = κₜ, κ = κₜ),
+    biogeochemistry,
+    auxiliary_fields = (; T, S),
+)
 
 set!(model, P1=0.01, P2=0.01, Z1=0.05, Z2=0.05, N=7.0, D=1)
 
-simulation = Simulation(model, Δt = 3minutes, stop_time = 100days)
+simulation = Simulation(model, Δt=3minutes, stop_time=100days)
 
 filename = "column"
-simulation.output_writers[:profiles] = JLD2OutputWriter(model, model.tracers,
-                                                        filename = "$filename.jld2",
-                                                        schedule = TimeInterval(1day),
-                                                        overwrite_existing = true)
+simulation.output_writers[:profiles] = JLD2Writer(
+    model,
+    model.tracers;
+    filename = "$filename.jld2",
+    schedule = TimeInterval(1day),
+    overwrite_existing = true,
+)
 
 run!(simulation)
