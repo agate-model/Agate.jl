2ea3c17f21bd686e6a27e4115e06e56a78c0b6ee
nanophyto <coralscaper@gmail.com>
2026-02-08 21:06:27 +0000

    reduce exports

--- src/Runtime/Runtime.jl
+++ src/Runtime/Runtime.jl
@@ -5,15 +5,6 @@ export TendencyContext
 export TracerValues
 export tendency_inputs
 
-export ClassRef
-export class
-export resolve_class
-export class_count
-
-export TracerIndex
-export Tracers
-export build_tracer_index
-
 include("tendency_context.jl")
 include("class_refs.jl")
 include("tracer_accessors.jl")
