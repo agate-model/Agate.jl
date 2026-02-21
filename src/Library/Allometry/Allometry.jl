d526875cbc595b86b1e84a309106ccfaa9420c31
nanophyto <coralscaper@gmail.com>
2026-02-21 16:42:13 +0000

    reduce public surface

--- src/Library/Allometry/Allometry.jl
+++ src/Library/Allometry/Allometry.jl
@@ -2,12 +2,10 @@
 module Allometry
 
 export AbstractParamDef, ConstantParam, AllometricParam
-export PowerLaw, resolve_param, cast_paramdef
-export resolve_diameter_vector, resolve_diameter_indexed_vector
+export PowerLaw
 export PalatabilityPreyParameters, PalatabilityPredatorParameters
 export allometric_scaling_power
 export allometric_palatability_unimodal, allometric_palatability_unimodal_protection
-export palatability_matrix_allometric_axes, assimilation_efficiency_matrix_binary_axes
 
 include("parameter_defs.jl")
 include("interactions.jl")
