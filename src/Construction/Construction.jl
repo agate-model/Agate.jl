20e720560e1c4a3474a07095698bd758521e366e
nanophyto <coralscaper@gmail.com>
2026-03-09 11:16:30 +0000

    move docstring to more relevant place

--- src/Construction/Construction.jl
+++ src/Construction/Construction.jl
@@ -1,18 +1,4 @@
-"""
-Model-agnostic biogeochemistry constructor.
-
-The public entry points are the **model constructors** exposed in `Agate.Models`
-(e.g. `Agate.Models.NiPiZD.NiPiZD(...)` and `Agate.Models.DARWIN.DARWIN(...)`).
-
-This module contains the model-agnostic implementation:
-
-```julia
-Agate.Construction.construct_factory(factory::AbstractBGCFactory; kwargs...) -> bgc
-```
-
-`construct_factory` returns the biogeochemistry instance directly.
-
-"""
+"""Model construction utilities for factory-defined biogeochemistry models."""
 module Construction
 
 import Oceananigans
@@ -24,4 +10,4 @@ export PFTSpecification
 include("generator.jl")
 include("construct.jl")
 
-end # module
+end
