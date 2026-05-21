78167898f405c0f6c6dc605cc95107befa9d51da
nanophyto <joost.c.devries@protonmail.com>
2026-05-21 09:20:01 +0100

    add ode_problem

--- src/Diagnostics/Diagnostics.jl
+++ src/Diagnostics/Diagnostics.jl
@@ -5,7 +5,10 @@ import Oceananigans: time_step!
 
 export box_model_budget
 export box_model_mass_balance
+export ode_problem
+export ode_initial_state
 
 include("box_model.jl")
+include("ode_problem.jl")
 
 end # module
