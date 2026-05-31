dc274d53792390f52ff7cb2d8796d328fbd29b24
nanophyto <joost.c.devries@protonmail.com>
2026-05-31 17:31:08 +0100

    less verbose tests

--- test/test_construction_manifest.jl
+++ test/test_construction_manifest.jl
@@ -1,4 +1,3 @@
-using Adapt
 using JSON
 using Oceananigans.Units: day
 using OceanBioME: BoxModelGrid
@@ -6,6 +5,30 @@ using Oceananigans.Biogeochemistry: required_biogeochemical_tracers
 
 struct UnsupportedManifestValue end
 
+function darwin_manifest_fixture()
+    kwargs = (;
+        grid=dummy_grid(Float32),
+        phyto_size_structure=(n=3, min_esd=1.5, max_esd=20.0, splitting=:log_splitting),
+        zoo_size_structure=(n=2, min_esd=20.0, max_esd=100.0, splitting=:log_splitting),
+    )
+
+    bgc = Agate.Models.DARWIN.construct(; kwargs...)
+    traced_bgc, manifest = Agate.Models.DARWIN.construct_with_manifest(; kwargs...)
+    return bgc, traced_bgc, manifest
+end
+
+function nipizd_manifest_fixture(; kwargs...)
+    bgc = Agate.Models.NiPiZD.construct(; kwargs...)
+    traced_bgc, manifest = Agate.Models.NiPiZD.construct_with_manifest(; kwargs...)
+    return bgc, traced_bgc, manifest
+end
+
+function test_manifest_roundtrip(manifest, expected_bgc; grid)
+    replayed = Agate.Manifests.construct_from_manifest(manifest; grid)
+    @test typeof(replayed) == typeof(expected_bgc)
+    @test required_biogeochemical_tracers(replayed) == required_biogeochemical_tracers(expected_bgc)
+    return replayed
+end
 
 @testset "Manifest value serialization" begin
     value = Agate.Construction.manifest_parameter_value
@@ -17,153 +40,164 @@ struct UnsupportedManifestValue end
     @test_throws ArgumentError value(UnsupportedManifestValue())
 end
 
-@testset "Model-level construction manifests" begin
-    darwin_bgc = Agate.Models.DARWIN.construct(
-        ;
-        grid=dummy_grid(Float32),
-        phyto_size_structure=(n=3, min_esd=1.5, max_esd=20.0, splitting=:log_splitting),
-        zoo_size_structure=(n=2, min_esd=20.0, max_esd=100.0, splitting=:log_splitting),
-    )
-    traced_darwin, darwin_manifest = Agate.Models.DARWIN.construct_with_manifest(
-        ;
-        grid=dummy_grid(Float32),
-        phyto_size_structure=(n=3, min_esd=1.5, max_esd=20.0, splitting=:log_splitting),
-        zoo_size_structure=(n=2, min_esd=20.0, max_esd=100.0, splitting=:log_splitting),
-    )
+@testset "DARWIN construction manifests" begin
+    darwin_bgc, traced_darwin, manifest = darwin_manifest_fixture()
 
     @test typeof(traced_darwin) == typeof(darwin_bgc)
-    @test darwin_manifest["model"]["id"] == "DARWIN/default"
-    @test darwin_manifest["model"]["family"] == "DARWIN"
-    @test !haskey(darwin_manifest, "replayable")
-    @test darwin_manifest["resolved"]["scalar_type"] == "Float32"
-    @test darwin_manifest["resolved"]["sinking"] == Dict{String,Any}(
+    @test manifest["model"]["id"] == "DARWIN/default"
+    @test manifest["model"]["family"] == "DARWIN"
+    @test !haskey(manifest, "replayable")
+    @test manifest["resolved"]["scalar_type"] == "Float32"
+    @test manifest["resolved"]["sinking"] == Dict{String,Any}(
         "enabled" => false, "tracers" => nothing, "open_bottom" => true
     )
-    @test "P3" in darwin_manifest["resolved"]["tracers"]
-    @test darwin_manifest["recipe"]["type"] == "model_constructor"
-    @test darwin_manifest["recipe"]["family"] == "DARWIN"
-    @test !haskey(darwin_manifest["recipe"], "constructor")
-    @test darwin_manifest["recipe"]["kwargs"]["phyto_size_structure"] ==
-        darwin_manifest["resolved"]["plankton_diameters_by_group"]["P"]
-    @test darwin_manifest["recipe"]["kwargs"]["zoo_size_structure"] ==
-        darwin_manifest["resolved"]["plankton_diameters_by_group"]["Z"]
-    @test darwin_manifest["recipe"]["kwargs"]["parameters"] isa Dict
-    @test darwin_manifest["recipe"]["kwargs"]["sinking_tracers"] === nothing
-    @test darwin_manifest["recipe"]["kwargs"]["open_bottom"] == true
-    @test darwin_manifest["recipe"]["kwargs"]["scalar_type"] == "Float32"
+    @test "P3" in manifest["resolved"]["tracers"]
+    @test manifest["recipe"]["type"] == "model_constructor"
+    @test manifest["recipe"]["family"] == "DARWIN"
+    @test !haskey(manifest["recipe"], "constructor")
+    @test manifest["recipe"]["kwargs"]["phyto_size_structure"] ==
+        manifest["resolved"]["plankton_diameters_by_group"]["P"]
+    @test manifest["recipe"]["kwargs"]["zoo_size_structure"] ==
+        manifest["resolved"]["plankton_diameters_by_group"]["Z"]
+    @test manifest["recipe"]["kwargs"]["parameters"] isa Dict
+    @test manifest["recipe"]["kwargs"]["sinking_tracers"] === nothing
+    @test manifest["recipe"]["kwargs"]["open_bottom"] == true
+    @test manifest["recipe"]["kwargs"]["scalar_type"] == "Float32"
+end
 
-    _, sinking_manifest = Agate.Models.NiPiZD.construct_with_manifest(
+@testset "NiPiZD construction manifests" begin
+    nipizd_bgc, traced_nipizd, manifest = nipizd_manifest_fixture(; grid=dummy_grid(Float32))
+
+    @test typeof(traced_nipizd) == typeof(nipizd_bgc)
+    @test manifest["model"]["id"] == "NiPiZD/default"
+    @test manifest["model"]["family"] == "NiPiZD"
+    @test !haskey(manifest, "replayable")
+    @test manifest["resolved"]["scalar_type"] == "Float32"
+    @test haskey(manifest["resolved"], "parameters")
+    @test manifest["recipe"]["type"] == "model_constructor"
+    @test manifest["recipe"]["family"] == "NiPiZD"
+    @test !haskey(manifest["recipe"], "constructor")
+    @test haskey(manifest["recipe"]["kwargs"], "parameters")
+end
+
+@testset "Sinking manifest entries" begin
+    _, manifest = Agate.Models.NiPiZD.construct_with_manifest(
         ;
         grid=BoxModelGrid(),
         sinking_tracers=(P1=0.2551 / day, D=2.7489 / day),
         open_bottom=false,
     )
-    @test sinking_manifest["resolved"]["has_sinking_velocities"]
-    @test sinking_manifest["resolved"]["sinking"]["enabled"]
-    @test sinking_manifest["resolved"]["sinking"]["open_bottom"] == false
-    @test sinking_manifest["resolved"]["sinking"]["tracers"] == Dict{String,Any}(
+
+    @test manifest["resolved"]["has_sinking_velocities"]
+    @test manifest["resolved"]["sinking"]["enabled"]
+    @test manifest["resolved"]["sinking"]["open_bottom"] == false
+    @test manifest["resolved"]["sinking"]["tracers"] == Dict{String,Any}(
         "P1" => 0.2551 / day, "D" => 2.7489 / day
     )
-    @test sinking_manifest["recipe"]["kwargs"]["sinking_tracers"] == Any[
+    @test manifest["recipe"]["kwargs"]["sinking_tracers"] == Any[
         Dict{String,Any}("name" => "P1", "value" => 0.2551 / day),
         Dict{String,Any}("name" => "D", "value" => 2.7489 / day),
     ]
-    @test sinking_manifest["recipe"]["kwargs"]["open_bottom"] == false
+    @test manifest["recipe"]["kwargs"]["open_bottom"] == false
+end
 
-    nipizd_bgc = Agate.Models.NiPiZD.construct(; grid=dummy_grid(Float32))
-    traced_nipizd, nipizd_manifest = Agate.Models.NiPiZD.construct_with_manifest(
-        ; grid=dummy_grid(Float32)
+@testset "Manifest replay" begin
+    darwin_bgc, _, darwin_manifest = darwin_manifest_fixture()
+    nipizd_bgc, _, nipizd_manifest = nipizd_manifest_fixture(; grid=dummy_grid(Float32))
+
+    test_manifest_roundtrip(darwin_manifest, darwin_bgc; grid=dummy_grid(Float32))
+    test_manifest_roundtrip(nipizd_manifest, nipizd_bgc; grid=dummy_grid(Float32))
+
+    sinking_bgc, sinking_manifest = Agate.Models.NiPiZD.construct_with_manifest(
+        ; grid=BoxModelGrid(), sinking_tracers=(P1=0.2551 / day, D=2.7489 / day), open_bottom=false
     )
+    replayed_sinking = Agate.Manifests.construct_from_manifest(sinking_manifest; grid=BoxModelGrid())
+    @test typeof(replayed_sinking) == typeof(sinking_bgc)
+end
 
-    @test typeof(traced_nipizd) == typeof(nipizd_bgc)
-    @test nipizd_manifest["model"]["id"] == "NiPiZD/default"
-    @test nipizd_manifest["model"]["family"] == "NiPiZD"
-    @test !haskey(nipizd_manifest, "replayable")
-    @test nipizd_manifest["resolved"]["scalar_type"] == "Float32"
-    @test haskey(nipizd_manifest["resolved"], "parameters")
-    @test nipizd_manifest["recipe"]["type"] == "model_constructor"
-    @test nipizd_manifest["recipe"]["family"] == "NiPiZD"
-    @test !haskey(nipizd_manifest["recipe"], "constructor")
-    @test haskey(nipizd_manifest["recipe"]["kwargs"], "parameters")
-
-    replayed_darwin = Agate.Manifests.construct_from_manifest(darwin_manifest; grid=dummy_grid(Float32))
-    @test typeof(replayed_darwin) == typeof(darwin_bgc)
-    @test required_biogeochemical_tracers(replayed_darwin) == required_biogeochemical_tracers(darwin_bgc)
-
-    replayed_nipizd = Agate.Manifests.construct_from_manifest(nipizd_manifest; grid=dummy_grid(Float32))
-    @test typeof(replayed_nipizd) == typeof(nipizd_bgc)
-
-    @test required_biogeochemical_tracers(replayed_nipizd) == required_biogeochemical_tracers(nipizd_bgc)
-
-    replayed_sinking_nipizd = Agate.Manifests.construct_from_manifest(sinking_manifest; grid=BoxModelGrid())
-    @test typeof(replayed_sinking_nipizd) == typeof(Agate.Models.NiPiZD.construct(; grid=BoxModelGrid(), sinking_tracers=(P1=0.2551 / day, D=2.7489 / day), open_bottom=false))
-
+@testset "Manifest file IO" begin
+    darwin_bgc, _, manifest = darwin_manifest_fixture()
     path = tempname() * ".json"
-    @test Agate.Manifests.export_manifest(path, darwin_manifest) == path
-    @test_throws ArgumentError Agate.Manifests.export_manifest(path, (; schema="x"))
-    replayed_from_path = Agate.Manifests.construct_from_manifest(path; grid=dummy_grid(Float32))
-    @test typeof(replayed_from_path) == typeof(darwin_bgc)
 
-    roundtrip_matrix = Float32[0.8 0.2; 0.3 0.7]
-    roundtrip_sinking = (P1=0.125f0 / day, D=1.5f0 / day)
-    roundtrip_bgc, roundtrip_manifest = Agate.Models.NiPiZD.construct_with_manifest(
+    @test Agate.Manifests.export_manifest(path, manifest) == path
+    @test_throws ArgumentError Agate.Manifests.export_manifest(path, (; schema="x"))
+
+    replayed = Agate.Manifests.construct_from_manifest(path; grid=dummy_grid(Float32))
+    @test typeof(replayed) == typeof(darwin_bgc)
+end
+
+@testset "Manifest roundtrip values" begin
+    matrix = Float32[0.8 0.2; 0.3 0.7]
+    sinking = (P1=0.125f0 / day, D=1.5f0 / day)
+    bgc, manifest = Agate.Models.NiPiZD.construct_with_manifest(
         ;
         grid=BoxModelGrid(),
         scalar_type=Float32,
-        palatability_matrix=roundtrip_matrix,
-        sinking_tracers=roundtrip_sinking,
+        palatability_matrix=matrix,
+        sinking_tracers=sinking,
         open_bottom=false,
     )
-    roundtrip_path = tempname() * ".json"
-    @test Agate.Manifests.export_manifest(roundtrip_path, roundtrip_manifest) == roundtrip_path
 
-    roundtrip_json = JSON.parsefile(roundtrip_path)
-    @test roundtrip_json["recipe"]["kwargs"]["scalar_type"] == "Float32"
-    @test roundtrip_json["recipe"]["kwargs"]["open_bottom"] == false
-    sinking_entries = roundtrip_json["recipe"]["kwargs"]["sinking_tracers"]
+    path = tempname() * ".json"
+    @test Agate.Manifests.export_manifest(path, manifest) == path
+
+    json = JSON.parsefile(path)
+    @test json["recipe"]["kwargs"]["scalar_type"] == "Float32"
+    @test json["recipe"]["kwargs"]["open_bottom"] == false
+
+    sinking_entries = json["recipe"]["kwargs"]["sinking_tracers"]
     @test getindex.(sinking_entries, "name") == ["P1", "D"]
     @test getindex.(sinking_entries, "value") ≈ [0.125f0 / day, 1.5f0 / day]
 
-    matrix_rows = roundtrip_json["recipe"]["kwargs"]["parameters"]["palatability_matrix"]
+    matrix_rows = json["recipe"]["kwargs"]["parameters"]["palatability_matrix"]
     decoded_matrix = [matrix_rows[i][j] for i in eachindex(matrix_rows), j in eachindex(matrix_rows[1])]
-    @test decoded_matrix ≈ roundtrip_matrix
+    @test decoded_matrix ≈ matrix
 
-    replayed_roundtrip = Agate.Manifests.construct_from_manifest(
-        roundtrip_path; grid=BoxModelGrid()
-    )
-    @test typeof(replayed_roundtrip) == typeof(roundtrip_bgc)
-    @test required_biogeochemical_tracers(replayed_roundtrip) ==
-        required_biogeochemical_tracers(roundtrip_bgc)
-    @test !isnothing(replayed_roundtrip.sinking_velocities)
-    @test hasproperty(replayed_roundtrip.sinking_velocities, :P1)
-    @test hasproperty(replayed_roundtrip.sinking_velocities, :D)
-    @test replayed_roundtrip.parameters.palatability_matrix ≈ roundtrip_matrix
+    replayed = Agate.Manifests.construct_from_manifest(path; grid=BoxModelGrid())
+    @test typeof(replayed) == typeof(bgc)
+    @test required_biogeochemical_tracers(replayed) == required_biogeochemical_tracers(bgc)
+    @test !isnothing(replayed.sinking_velocities)
+    @test hasproperty(replayed.sinking_velocities, :P1)
+    @test hasproperty(replayed.sinking_velocities, :D)
+    @test replayed.parameters.palatability_matrix ≈ matrix
+end
+
+@testset "Manifest validation errors" begin
+    _, _, manifest = darwin_manifest_fixture()
 
     @test_throws ArgumentError Agate.Manifests.construct_from_manifest(Dict{String,Any}())
-    @test_throws ArgumentError Agate.Manifests.construct_from_manifest(Dict{String,Any}("schema" => "agate.construction_manifest.v2"))
-    @test_throws ArgumentError Agate.Manifests.construct_from_manifest(Dict{String,Any}("schema" => "agate.construction_manifest.v1"))
+    @test_throws ArgumentError Agate.Manifests.construct_from_manifest(
+        Dict{String,Any}("schema" => "agate.construction_manifest.v2")
+    )
+    @test_throws ArgumentError Agate.Manifests.construct_from_manifest(
+        Dict{String,Any}("schema" => "agate.construction_manifest.v1")
+    )
 
-    missing_type_manifest = deepcopy(darwin_manifest)
+    missing_type_manifest = deepcopy(manifest)
     delete!(missing_type_manifest["recipe"], "type")
-    @test_throws ArgumentError Agate.Manifests.construct_from_manifest(missing_type_manifest; grid=dummy_grid(Float32))
+    @test_throws ArgumentError Agate.Manifests.construct_from_manifest(
+        missing_type_manifest; grid=dummy_grid(Float32)
+    )
 
-    unsupported_type_manifest = deepcopy(darwin_manifest)
+    unsupported_type_manifest = deepcopy(manifest)
     unsupported_type_manifest["recipe"]["type"] = "factory_graph"
-    @test_throws ArgumentError Agate.Manifests.construct_from_manifest(unsupported_type_manifest; grid=dummy_grid(Float32))
+    @test_throws ArgumentError Agate.Manifests.construct_from_manifest(
+        unsupported_type_manifest; grid=dummy_grid(Float32)
+    )
 
-    missing_family_manifest = deepcopy(darwin_manifest)
+    missing_family_manifest = deepcopy(manifest)
     delete!(missing_family_manifest["recipe"], "family")
     @test_throws ArgumentError Agate.Manifests.construct_from_manifest(
         missing_family_manifest; grid=dummy_grid(Float32)
     )
 
-    unsupported_family_manifest = deepcopy(darwin_manifest)
+    unsupported_family_manifest = deepcopy(manifest)
     unsupported_family_manifest["recipe"]["family"] = "NPZD"
     @test_throws ArgumentError Agate.Manifests.construct_from_manifest(
         unsupported_family_manifest; grid=dummy_grid(Float32)
     )
 
-    constructor_only_manifest = deepcopy(darwin_manifest)
+    constructor_only_manifest = deepcopy(manifest)
     delete!(constructor_only_manifest["recipe"], "family")
     constructor_only_manifest["recipe"]["constructor"] = "Agate.Models.DARWIN.construct"
     @test_throws ArgumentError Agate.Manifests.construct_from_manifest(
