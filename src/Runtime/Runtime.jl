17d940a67f888f78d18e2aa58a2933354d0cf545
nanophyto <joost.c.devries@protonmail.com>
2026-06-07 21:13:16 +0100

    include ode_problem in runtime export

--- src/Runtime/Runtime.jl
+++ src/Runtime/Runtime.jl
@@ -1,15 +1,17 @@
 # Runtime and diagnostic utilities.
 module Runtime
 
-using Oceananigans.Biogeochemistry: AbstractContinuousFormBiogeochemistry
+using Oceananigans.Biogeochemistry: AbstractContinuousFormBiogeochemistry, required_biogeochemical_auxiliary_fields, required_biogeochemical_tracers
 
 export TendencyContext
 export TracerValues
 export tendency_inputs
 export parameterized
+export ode_problem
 
 include("tendency_context.jl")
 include("active_parameters.jl")
+include("ode_problem.jl")
 include("class_refs.jl")
 include("tracer_accessors.jl")
 
