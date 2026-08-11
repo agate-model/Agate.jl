e24afe80d733856a5b2121907ef3c185beefd44d
nanophyto <joost.c.devries@protonmail.com>
2026-08-11 13:35:32 +0100

    remove unneeded import

--- test/test_library.jl
+++ test/test_library.jl
@@ -5,7 +5,6 @@ using ForwardDiff
 using Agate.Library.Nutrients: liebig_minimum, smooth_liebig_minimum
 using Agate.Library.Photosynthesis: liebig_nutrient_limitation, smooth_liebig_nutrient_limitation
 using Agate.Library.Predation: holling_type_ii, idealized_predation_loss
-using Agate.Tendencies: TendencyConfig
 
 @testset "Library" begin
     @test holling_type_ii(1.0, 1.0) == 0.5
@@ -13,3 +12,4 @@ using Agate.Tendencies: TendencyConfig
     loss = idealized_predation_loss(1.0, 0.5, 0.1, 0.2)
     @test loss > 0
 end
+
