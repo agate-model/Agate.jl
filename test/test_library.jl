0f0b929f25ba8ff9036508af0cdc0e5b92c8e997
Joost de Vries <36844534+nanophyto@users.noreply.github.com>
2026-08-12 14:12:50 +0100

    Numerical robustness and type genericity (#241)
    * Improve type conservation by replacing 50.0 with 50
    
    * add type checks to relevant library functions
    
    * fix imports
    
    * fix NaN handling of Liebig minimum
    
    * make box model budget type agnostic
    
    * increase model test integration and add float 32 test
    
    * bump to 0.8.3
    
    * make liebig min branchless

--- test/test_library.jl
+++ test/test_library.jl
@@ -13,3 +13,17 @@ using Agate.Library.Predation: holling_type_ii, idealized_predation_loss
     @test loss > 0
 end
 
+@testset "Library scalar genericity" begin
+    T = Float32
+
+    @test Agate.Library.Nutrients.monod_limitation(T(1), T(0.5)) isa T
+    @test Agate.Library.Nutrients.smooth_liebig_minimum(T(0.2), T(0.4)) isa T
+    @test Agate.Library.Photosynthesis.smooth_liebig_nutrient_limitation(
+        (T(1), T(2)), (T(0.5), T(1)), one(T)
+    ) isa T
+    @test Agate.Library.Photosynthesis.smith_light_limitation(T(50), T(0.1), T(1)) isa T
+    @test Agate.Library.Mortality.linear_loss(T(1), T(0.1)) isa T
+    @test Agate.Library.Predation.holling_type_ii(T(1), T(0.5)) isa T
+    @test Agate.Library.Remineralization.linear_remineralization(T(1), T(0.1)) isa T
+    @test Agate.Library.Temperature.q10_temperature_factor(T(10), T(2)) isa T
+end
