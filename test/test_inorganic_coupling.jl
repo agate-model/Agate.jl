b4600c667060f65c2bcc6681229afbee84145869
nanophyto <joost.c.devries@protonmail.com>
2026-08-18 17:21:22 +0100

    Remove inorganic coupling logging assertion

--- test/test_inorganic_coupling.jl
+++ test/test_inorganic_coupling.jl
@@ -1,14 +1,9 @@
 using Agate
 using Test
-using Logging
 
 @testset "Inorganic coupling" begin
     config = MULTI_NUTRIENT_LIEBIG
     sources = ((:DOC, :organic_remineralization), (:POC, :organic_remineralization))
-
-    @test_logs min_level=Logging.Warn Agate.Tendencies.inorganic_tendency(
-        config; target=:DIC, remineralization=sources
-    )
 
     @test Agate.Tendencies.inorganic_target_coupling(
         config, :DIC, sources, nothing
