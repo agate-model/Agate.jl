1e0caac3318897e63816c1786b314fb59a55b7e1
nanophyto <joost.c.devries@protonmail.com>
2026-08-19 17:11:45 +0100

    include process normalization tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,6 +9,7 @@ include("test_tracer_functions.jl")
 include("test_classrefs_and_tracer_accessors.jl")
 include("test_library.jl")
 include("test_components.jl")
+include("test_processes.jl")
 include("test_inorganic_coupling.jl")
 include("test_box_model.jl")
 
