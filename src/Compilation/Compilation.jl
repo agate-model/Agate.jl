f5eba5d34e947dfb0c51d1979361742e952c2677
nanophyto <joost.c.devries@protonmail.com>
2026-08-19 20:01:32 +0100

    expose complete process model compiler

--- src/Compilation/Compilation.jl
+++ src/Compilation/Compilation.jl
@@ -8,12 +8,14 @@ using ..Processes:
     Growth,
     Grazing,
     Mortality,
+    Remineralization,
     Monod,
     NamedProcess,
     PreferentialGrazing,
     NormalizedModelDefinition,
     NutrientResponse,
     PartitionRouting,
+    LinearRemineralization,
     Smith,
     formulation,
     parameter_name,
@@ -29,13 +31,16 @@ export GrazingResourceLossContribution, GrazingConsumerGainContribution
 export GrazingUnassimilatedContribution
 export MortalityParameterBinding, MortalityTopology
 export MortalityLossContribution, MortalityRoutingContribution
+export RemineralizationParameterBinding, RemineralizationTopology
+export RemineralizationSourceLossContribution, RemineralizationDestinationGainContribution
 export realize_process_topology, process_contributions
 export contribution_target, group_contributions
-export compile_tendency, compile_tendencies
+export model_contributions, compile_tendency, compile_tendencies, compile_model_tendencies
 
 include("contributions.jl")
 include("mortality.jl")
 include("growth.jl")
 include("grazing.jl")
+include("remineralization.jl")
 
 end # module Compilation
