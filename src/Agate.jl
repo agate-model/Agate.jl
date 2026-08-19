7d41f99ecbb90578ad8745852f657641cdffa27c
nanophyto <joost.c.devries@protonmail.com>
2026-08-19 17:11:31 +0100

    expose process authoring API

--- src/Agate.jl
+++ src/Agate.jl
@@ -4,6 +4,7 @@ include("Factories/Factories.jl")
 include("Equations/Equations.jl")
 include("Library/Library.jl")
 include("Configuration/Configuration.jl")
+include("Processes/Processes.jl")
 include("Runtime/Runtime.jl")
 include("Diagnostics/Diagnostics.jl")
 include("Construction/Construction.jl")
@@ -15,6 +16,7 @@ export Library
 export Models
 export Factories
 export Configuration
+export Processes
 export Runtime
 export Diagnostics
 export Equations
