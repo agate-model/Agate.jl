cfc37cf907be5762227f0301393389e01eebe73d
nanophyto <joost.c.devries@protonmail.com>
2026-08-19 17:46:09 +0100

    expose canonical process rate interface

--- src/Processes/Processes.jl
+++ src/Processes/Processes.jl
@@ -4,6 +4,7 @@ module Processes
 using ..Configuration: Population, Pool
 using ..Factories:
     AbstractBGCFactory, default_components, default_processes, parameter_definitions
+using ..Library.Mortality: linear_loss, quadratic_loss
 
 export AbstractProcess, AbstractFormulation
 export Smith, Monod, PreferentialGrazing
@@ -13,8 +14,10 @@ export ModelDefinition, NormalizedModelDefinition, NamedProcess
 export ParameterRequirementIdentity
 export process_id, process_kind, formulation, formulation_tag
 export participants, drivers, rate_axes, driver_identities, normalize_model
+export process_rate
 
 include("authoring.jl")
+include("rates.jl")
 include("normalization.jl")
 
 end # module Processes
