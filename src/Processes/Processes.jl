9a07be9e4de1eeec326d03f748316181876f83aa
nanophyto <joost.c.devries@protonmail.com>
2026-08-19 19:13:31 +0100

    expose parameter binding interface

--- src/Processes/Processes.jl
+++ src/Processes/Processes.jl
@@ -1,9 +1,14 @@
 """Scientific process authoring and setup-time normalization."""
 module Processes
 
-using ..Configuration: Population, Pool
+using ..Configuration: Population, Pool, ComponentLayout
 using ..Factories:
-    AbstractBGCFactory, default_components, default_processes, parameter_definitions
+    AbstractBGCFactory,
+    ParameterDefinition,
+    ParameterProvision,
+    default_components,
+    default_processes,
+    parameter_definitions
 using ..Library.Mortality: linear_loss, quadratic_loss
 
 export AbstractProcess, AbstractFormulation
@@ -11,9 +16,12 @@ export Smith, Monod, PreferentialGrazing
 export LinearMortality, QuadraticMortality, LinearRemineralization, PartitionRouting
 export Growth, NutrientResponse, Grazing, Mortality, ProductRouting, Remineralization
 export ModelDefinition, NormalizedModelDefinition, NamedProcess
-export ParameterRequirementIdentity
+export ParameterRequirementIdentity, ParameterRequirement
+export ParameterBinding, ParameterApplicability
 export process_id, process_kind, formulation, formulation_tag
 export participants, drivers, rate_axes, driver_identities, normalize_model
+export parameter_requirements, parameter_bindings, parameter_name
+export resolve_parameter_applicability
 export process_rate
 
 include("authoring.jl")
