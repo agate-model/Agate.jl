2952ea9152ce22118057887c4a9ff556d55afe69
nanophyto <joost.c.devries@protonmail.com>
2026-08-19 20:01:12 +0100

    integrate growth with model contribution compilation

--- src/Compilation/growth.jl
+++ src/Compilation/growth.jl
@@ -123,6 +123,17 @@ function process_contributions(
     return contributions
 end
 
+function process_contributions(
+    named::NamedProcess{P},
+    definition::NormalizedModelDefinition,
+    layout::ComponentLayout,
+    context::CommunityContext,
+) where {P<:Growth}
+    topology = realize_process_topology(named, layout, context)
+    binding = GrowthParameterBinding(definition, process_id(named))
+    return process_contributions(named, topology, binding)
+end
+
 struct GrowthTerm{F,L,I,R,D,M,K,A,Effect}
     formulation::F
     limitation::L
