bc3adc0b2ccd3183ff7d1e46cbf70a18f194375d
nanophyto <joost.c.devries@protonmail.com>
2026-08-19 19:35:32 +0100

    add Smith Monod process rate dispatch

--- src/Processes/rates.jl
+++ src/Processes/rates.jl
@@ -3,3 +3,40 @@
 @inline process_rate(::QuadraticMortality, biomass, coefficient) = quadratic_loss(biomass, coefficient)
 @inline process_rate(process::Mortality, biomass, coefficient) =
     process_rate(formulation(process), biomass, coefficient)
+
+"""Evaluate Smith growth with one Monod nutrient response."""
+@inline function process_rate(
+    ::Smith,
+    ::Monod,
+    biomass,
+    resource,
+    light,
+    maximum_rate,
+    half_saturation,
+    alpha,
+)
+    return smith_growth(
+        (resource,), biomass, light, maximum_rate, (half_saturation,), alpha
+    )
+end
+
+@inline function process_rate(
+    process::Growth{Smith,L},
+    biomass,
+    resource,
+    light,
+    maximum_rate,
+    half_saturation,
+    alpha,
+) where {L<:NutrientResponse{Monod}}
+    return process_rate(
+        process.formulation,
+        process.limitation.formulation,
+        biomass,
+        resource,
+        light,
+        maximum_rate,
+        half_saturation,
+        alpha,
+    )
+end
